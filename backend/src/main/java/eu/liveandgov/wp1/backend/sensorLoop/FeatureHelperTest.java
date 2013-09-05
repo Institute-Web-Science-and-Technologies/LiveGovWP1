05d70dcb7f36406b1f4a6e918ae0a5d3328982dd
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-09-05 11:14:24 +0200

    Extended test

--- backend/src/main/java/eu/liveandgov/wp1/backend/sensorLoop/FeatureHelperTest.java
+++ backend/src/main/java/eu/liveandgov/wp1/backend/sensorLoop/FeatureHelperTest.java
@@ -2,6 +2,7 @@ package eu.liveandgov.wp1.backend.sensorLoop;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.math3.complex.Complex;
 import org.junit.Test;
 
 public class FeatureHelperTest {
@@ -13,7 +14,12 @@ public class FeatureHelperTest {
 		assertEquals( 3, FeatureHelper.mean(input), 0.005 );
 		
 		System.out.println(FeatureHelper.var(input));
-		
+
+		input = new float[] { 1,2,3,4,5,6,7,8 };
+
+		for (Complex s : FeatureHelper.FFT(input)){
+			System.out.println(s);
+		}
 	}
 
 }
