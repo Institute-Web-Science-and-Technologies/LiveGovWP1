2a7c1906a169a09abcc2e1d753c5d964beec427a
Richard Schütz <rschuetz@uni-koblenz.de>
2013-09-05 12:27:47 +0200

    Added sensor value objects for GPS and tags

--- backend/src/main/java/eu/liveandgov/wp1/backend/SensorValueObjects/SensorValue.java
+++ backend/src/main/java/eu/liveandgov/wp1/backend/SensorValueObjects/SensorValue.java
@@ -2,7 +2,7 @@ package eu.liveandgov.wp1.backend.SensorValueObjects;
 
 import eu.liveandgov.wp1.backend.format.SampleType;
 
-public class SensorValue {
+public abstract class SensorValue {
 	public SampleType type;
 	public long timestamp;
 	public String id;
