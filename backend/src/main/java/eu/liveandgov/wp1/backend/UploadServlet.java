337b55620e53d2d58fcd6f9d60c21dcf17638b89
Richard Schütz <rschuetz@uni-koblenz.de>
2013-09-05 15:49:36 +0200

    Begun database insertion

--- backend/src/main/java/eu/liveandgov/wp1/backend/UploadServlet.java
+++ backend/src/main/java/eu/liveandgov/wp1/backend/UploadServlet.java
@@ -1,5 +1,6 @@
 package eu.liveandgov.wp1.backend;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -8,17 +9,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import eu.liveandgov.wp1.backend.SensorValueObjects.AccSensorValue;
+import eu.liveandgov.wp1.backend.SensorValueObjects.GPSSensorValue;
+import eu.liveandgov.wp1.backend.SensorValueObjects.RawSensorValue;
+import eu.liveandgov.wp1.backend.SensorValueObjects.TagSensorValue;
 
 /**
  * Servlet implementation class UploadServlet2
@@ -81,7 +92,12 @@ public class UploadServlet extends HttpServlet {
 		copyStream(uploadedFileInputStream, baos);
 		saveToDisk(new ByteArrayInputStream(baos.toByteArray()), request.getHeader("id"));
 		
-//		saveToDatabase(new ByteArrayInputStream(baos.toByteArray()));
+		try {
+			saveToDatabase(new ByteArrayInputStream(baos.toByteArray()));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void saveToDisk(InputStream input, String id) throws IOException {
@@ -92,6 +108,59 @@ public class UploadServlet extends HttpServlet {
 		copyStream(input, outstream);
 		outstream.flush();
 		outstream.close();
+	}
+	
+	private void saveToDatabase(InputStream input) throws IOException, UnavailableException, SQLException {
+		PostgresqlDatabase db = new PostgresqlDatabase("user", "pass");
+		PreparedStatement psAcc, psGPS, psTag;
+		Timestamp ts;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		
+		psAcc = db.connection.prepareStatement("INSERT INTO accelerometer VALUES (?, ?, ?, ?, ?)");
+		psGPS = db.connection.prepareStatement("INSERT INTO gps VALUES (?, ?, ?, ?)");
+		psTag = db.connection.prepareStatement("INSERT INTO tags VALUES (?, ?, ?)");
+		
+		while (reader.ready()) {
+			RawSensorValue rsv = RawSensorValue.fromString(reader.readLine());
+			switch (rsv.type) {
+			case ACC:
+				AccSensorValue asv = AccSensorValue.fromRSV(rsv);
+				psAcc.setString(1, asv.id);
+				ts = new Timestamp(asv.timestamp * 1000L);
+				psAcc.setTimestamp(2, ts);
+				psAcc.setFloat(3, asv.x);
+				psAcc.setFloat(4, asv.y);
+				psAcc.setFloat(5, asv.z);
+				psAcc.addBatch();
+				break;
+			case GPS:
+				GPSSensorValue gsv = GPSSensorValue.fromRSV(rsv);
+				psGPS.setString(1, gsv.id);
+				ts = new Timestamp(gsv.timestamp * 1000L);
+				psGPS.setTimestamp(2, ts);
+				// TODO
+				psGPS.setDouble(3, gsv.latitude);
+				psGPS.setDouble(4, gsv.longitude);
+				psGPS.addBatch();
+				break;
+			case TAG:
+				TagSensorValue tsv = TagSensorValue.fromRSV(rsv);
+				psTag.setString(1, tsv.id);
+				ts = new Timestamp(tsv.timestamp * 1000L);
+				psTag.setTimestamp(2, ts);
+				psTag.setString(3, tsv.tag);
+				psTag.addBatch();
+				break;
+			default:
+				break;
+			}
+		}
+		psAcc.executeBatch();
+		psGPS.executeBatch();
+		psTag.executeBatch();
+		psAcc.close();
+		psGPS.close();
+		psTag.close();
 	}
 	
 
