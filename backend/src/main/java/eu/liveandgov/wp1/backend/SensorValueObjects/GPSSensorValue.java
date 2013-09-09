b2d89c17cebc399c575ec4d4002f6334d9f3a061
Richard Schütz <rschuetz@uni-koblenz.de>
2013-09-09 13:54:57 +0200

    Database upload

--- backend/src/main/java/eu/liveandgov/wp1/backend/SensorValueObjects/GPSSensorValue.java
+++ backend/src/main/java/eu/liveandgov/wp1/backend/SensorValueObjects/GPSSensorValue.java
@@ -7,6 +7,7 @@ public class GPSSensorValue extends SensorValue {
 	public final SampleType type = SampleType.GPS;
 	public double latitude;
 	public double longitude;
+	public double altitude;
 
 	public static GPSSensorValue fromRSV(RawSensorValue rsv) {
 		GPSSensorValue out = new GPSSensorValue();
@@ -15,19 +16,20 @@ public class GPSSensorValue extends SensorValue {
 		out.id = rsv.id;
 
 		String[] stringValues = rsv.value.split(" ");
-		if (stringValues.length != 2)
+		if (stringValues.length != 3)
 			throw new IllegalArgumentException("Cannot parse value "
 					+ rsv.value);
 
 		out.longitude = Double.parseDouble(stringValues[0]);
 		out.latitude = Double.parseDouble(stringValues[1]);
+		out.altitude = Double.parseDouble(stringValues[2]);
 
 		return out;
 	}
 
 	public String toString() {
-		return String.format("GSV - type:%s ts:%d id:%s lon:%f lat:%f", type,
-				timestamp, id, longitude, latitude);
+		return String.format("GSV - type:%s ts:%d id:%s lon:%f lat:%f alt:%f", type,
+				timestamp, id, longitude, latitude, altitude);
 	}
 
 }
