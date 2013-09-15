47a9d85573c97dcae3cf6092dd873a34adb35595
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-09-15 15:24:53 +0200

    Added TransferThread Test

--- LiveAndGovCollectorProject/LiveAndGovCollector/src/main/java/eu/liveandgov/wp1/collector/transfer/TransferTest.java
+++ LiveAndGovCollectorProject/LiveAndGovCollector/src/main/java/eu/liveandgov/wp1/collector/transfer/TransferTest.java
@@ -1,25 +1,16 @@
 package eu.liveandgov.wp1.collector.transfer;
 
 import android.net.ConnectivityManager;
-
-import eu.liveandgov.wp1.collector.persistence.MockPersister;
-import eu.liveandgov.wp1.collector.persistence.PersistenceInterface;
+import android.util.Log;
 
 /**
  * Created by hartmann on 9/13/13.
  */
 public class TransferTest {
-
-    public TransferTest(ConnectivityManager CM){
-        PersistenceInterface PI = new MockPersister();
-
-        TransferInterface t = new TransferThread(CM, PI);
-
-        for (int i = 0; i < 100; i++){
-            PI.save("SAMPLE " + i);
-        }
-
-
-
+    public TransferTest(){
+        Log.i("TEST", "Testing Network");
+        TransferThread T = new TransferThread();
+        T.transferData("Hello World");
     }
+
 }
