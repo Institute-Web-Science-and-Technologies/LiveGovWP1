7913b6fddd9a7e9647b3d5a3046371c7624abad1
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-10-09 21:07:29 +0200

    Cleand working directory.
    Removed SensorCollectorProject

--- SensorCollectorProject/SensorCollector/build/source/buildConfig/debug/eu/liveandgov/wp1/sensorcollector/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package eu.liveandgov.wp1.sensorcollector;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
