b089afbb1a4f7d44ce8d86a16f3b584cfe41ec5f
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-10-10 16:08:27 +0200

    added command line parameters to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,20 @@
 WEBAPPS=/var/lib/tomcat7/webapps/
-PROJECT_ROOT=`dirname $0`/..
 
-echo "Deploying project $PROJECT_ROOT"
-cd $PROJECT_ROOT/backend
+BRANCH="dev"
+if [ -n "$1" ] 
+then
+    BRANCH="$1";
+fi
+
+
+SCRIPT_FOLDER=`dirname $0`
+PROJECT_ROOT="$SCRIPT_FOLDER/../backend"
+echo "Deploying $PROJECT_ROOT"
+cd $PROJECT_ROOT
+echo `pwd`
+
+echo "Pulling branch $BRANCH from github.com"
+git pull origin $BRANCH
 
 echo "Running: mvn package war:war"
 mvn package war:war > deploy.log
