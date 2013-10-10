9264938ab93d73367eeb518c8e91b860e1c0eb35
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-10-10 14:01:55 +0200

    Fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 WEBAPPS=/var/lib/tomcat7/webapps/
-PROJECT_ROOT=`dirname $0`
+PROJECT_ROOT=`dirname $0`/..
 
 echo "Deploying project $PROJECT_ROOT"
-cd $PROJECT_ROOT
+cd $PROJECT_ROOT/backend
 
 echo "Running: mvn package war:war"
 mvn package war:war > deploy.log
