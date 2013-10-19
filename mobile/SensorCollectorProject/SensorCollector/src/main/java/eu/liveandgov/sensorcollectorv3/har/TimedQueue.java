0ef29c5dc3e39d2492a72adc0314ed3473f07773
Heinrich Hartmann <hartmann@uni-koblenz.de>
2013-10-19 16:42:19 +0200

    merged TimeQueue

--- mobile/SensorCollectorProject/SensorCollector/src/main/java/eu/liveandgov/sensorcollectorv3/har/TimedQueue.java
+++ mobile/SensorCollectorProject/SensorCollector/src/main/java/eu/liveandgov/sensorcollectorv3/har/TimedQueue.java
@@ -1,24 +1,36 @@
 package eu.liveandgov.sensorcollectorv3.har;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
+ * Class that holds samples in a time frame of a given duration d.
+ * The time frame ends with the time stamp t of the last sample that was pushed to the queue
+ * and starts after the time t - d (exclusively).
+ *
+ *  Time                    ---(t-d)--------t-->
+ *  Samples:                + + | + + + + + + |
+ *                                            Last Sample in queue
+ *  Contents of Queue:            + + + + + +
+ *  Removed samples:        + +
+ *
  * Created by cehlen on 10/18/13.
  */
 public class TimedQueue<V> {
-    private long size;
-    private Queue<TimeQueueEntry<V>> queue;
+    private final long duration;
+    private final Queue<TimeQueueEntry<V>> queue = new LinkedList<TimeQueueEntry<V>>();
 
-    public TimedQueue(long size) {
-        this.size = size;
-        queue = new LinkedList<TimeQueueEntry<V>>();
+    /**
+     * Creates a TimedQueue of a given duration
+     * @param duration
+     */
+    public TimedQueue(long duration) {
+        this.duration = duration;
     }
 
     /**
-     * Adds the new element to the queue and removes every entry which is older than time-size
+     * Adds the new element to the queue and removes every entry which is older than time-duration
      * @param time The time of the data point
      * @param value The sensor value
      */
@@ -42,13 +54,22 @@ public class TimedQueue<V> {
 
     /**
      * Checks if the queue is too big and removes elements if it has to.
-     * @param time The time from which we subtract the queue size
+     * @param time The time from which we subtract the queue duration
      */
     private void limitToSize(long time) {
-        long minTime = time - size;
+        long minTime = time - duration;
         while(queue.peek()!=null && queue.peek().time <= minTime) {
             queue.poll();
         }
+    }
+
+    /**
+     * Internal helper class that holds the queue entries.
+     * @param <V>
+     */
+    private static class TimeQueueEntry<V> {
+        public long time;
+        public V value;
     }
 
 }
