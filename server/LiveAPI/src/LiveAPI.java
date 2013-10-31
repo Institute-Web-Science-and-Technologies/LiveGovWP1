221d4a60a669850012c8219e1de103ffba4b8b56
Christoph Schaefer <chrisschaefer@uni-koblenz.de>
2013-10-31 11:45:19 +0100

    just renamings

--- server/LiveAPI/src/LiveAPI.java
+++ server/LiveAPI/src/LiveAPI.java
@@ -26,16 +26,16 @@ public class LiveAPI {
 
 		class VehicleDistanceComparator implements Comparator<VehicleInfo> {
 
-			    static final double _equatorialEarthRadius = 6371009D; 
-			    static final double _d2r = (Math.PI / 180D);
+			    static final double equatorialEarthRadius = 6371009D; 
+			    static final double d2r = (Math.PI / 180D);
 
-			    public double HaversineInKM(double lat1, double long1, double lat2, double long2) {
-			        double dlong = (long2 - long1) * _d2r;
-			        double dlat = (lat2 - lat1) * _d2r;
-			        double a = Math.pow(Math.sin(dlat / 2D), 2D) + Math.cos(lat1 * _d2r) * Math.cos(lat2 * _d2r)
+			    public double haversineInMeter(double lat1, double long1, double lat2, double long2) {
+			        double dlong = (long2 - long1) * d2r;
+			        double dlat = (lat2 - lat1) * d2r;
+			        double a = Math.pow(Math.sin(dlat / 2D), 2D) + Math.cos(lat1 * d2r) * Math.cos(lat2 * d2r)
 			                * Math.pow(Math.sin(dlong / 2D), 2D);
 			        double c = 2D * Math.atan2(Math.sqrt(a), Math.sqrt(1D - a));
-			        double d = _equatorialEarthRadius * c;
+			        double d = equatorialEarthRadius * c;
 
 			        return d;
 			    }
@@ -43,7 +43,7 @@ public class LiveAPI {
 
 			
 			private double calcDistance(VehicleInfo o) {
-				return HaversineInKM(lat, lon, o.getLat(), o.getLon());
+				return haversineInMeter(lat, lon, o.getLat(), o.getLon());
 			}
 
 			@Override
