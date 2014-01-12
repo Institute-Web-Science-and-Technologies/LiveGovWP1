565011506da2430ff657aad2e65aa743fc8c07da
Lukas Härtel <lukashaertel@googlemail.com>
2014-01-12 18:39:27 +0100

    Trying to get android tests to work under android studio

---
+++ mobile/SensorCollectorProject/SensorCollectorLibrary/src/instrumentTest/java/ServiceSensorControllerTest.java
@@ -0,0 +1,13 @@
+import android.test.ServiceTestCase;
+
+import eu.liveandgov.wp1.sensor_collector.ServiceSensorControl;
+
+/**
+ * Created by lukashaertel on 12.01.14.
+ */
+public class ServiceSensorControllerTest extends ServiceTestCase<ServiceSensorControl> {
+    public ServiceSensorControllerTest()
+    {
+        super(ServiceSensorControl.class);
+    }
+}
