042bec4ca1374ad4c97356113bee7fc25cf87ebd
Christoph Schaefer <chrisschaefer@uni-koblenz.de>
2014-01-13 18:42:17 +0100

    service center integration. work in progress

--- server/backend/src/main/java/eu/liveandgov/wp1/backend/SSCClient.java
+++ server/backend/src/main/java/eu/liveandgov/wp1/backend/SSCClient.java
@@ -1,6 +1,12 @@
 package eu.liveandgov.wp1.backend;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Authenticator;
 import java.net.HttpURLConnection;
@@ -16,6 +22,7 @@ import org.json.JSONObject;
 public class SSCClient {
 
 	private static final String REQUEST_BASE = "https://testservicecenter.yucat.com/servicecenter/api/";
+	// https://mobility.yucat.com/servicecenter/api
 
 	public SSCClient(final String user, final String pass) {
 		Authenticator.setDefault(new Authenticator() {
@@ -34,13 +41,15 @@ public class SSCClient {
 		GET, POST, PUT, DELETE
 	}
 
-	private HttpURLConnection prepareHttpURLConnection(String url, Method method)
+	private HttpURLConnection prepareHttpURLConnection(String url, Method method, String contentType)
 			throws IOException {
 		HttpURLConnection conn = (HttpURLConnection) new URL(REQUEST_BASE + url)
 				.openConnection();
-		if (method.equals(Method.POST) || method.equals(Method.PUT))
+		if (method.equals(Method.POST) || method.equals(Method.PUT)) {
 			conn.setDoOutput(true);
+		}
 		conn.setRequestMethod(method.toString());
+		conn.setRequestProperty("Content-Type", contentType);
 		conn.connect();
 		return conn;
 	}
@@ -51,13 +60,92 @@ public class SSCClient {
 		return df.format(date);
 	}
 
-	public void setHealthCheck(String id, Status status) throws IOException {
+	public String setHealthCheck(String id, Status status) throws IOException {
 		String request = "diagnostics/healthcheck/" + id;
 		JSONObject json = new JSONObject();
 		json.put("status", status.ordinal()).put("datelastupdate",
 				getISO8601UTCDate(new Date()));
-		HttpURLConnection conn = prepareHttpURLConnection(request, Method.PUT);
-		new OutputStreamWriter(conn.getOutputStream()).write(json.toString());
+		HttpURLConnection conn = prepareHttpURLConnection(request, Method.PUT, "application/json");
+	
+		OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+		out.write(json.toString());
+		out.close();
+		return readResponse(conn);
+	}
+	
+	private String readResponse(HttpURLConnection conn) throws IOException {
+		if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String decodedString;
+			String returnVal = "";
+			while ((decodedString = in.readLine()) != null) {
+				returnVal += decodedString;
+			}
+			in.close();
+			return returnVal;
+		}
+		else {
+			return "code: " + conn.getResponseCode();
+		}
 	}
 
+	public String postLogFile(String id, String filename) throws IOException {
+
+		String crlf = "\r\n";
+		String twoHyphens = "--";
+		String boundary =  "---------------------------LIVEANDGOV---";
+		
+		String request = "diagnostics/log/" + id;
+		
+		JSONObject json = new JSONObject();
+		json.put("filename", filename).put("customerid",1).put("registereduserid",1);
+		
+		HttpURLConnection conn = prepareHttpURLConnection(request, Method.POST, "multipart/form-data;boundary=" + boundary);
+
+		DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+		out.writeBytes(twoHyphens + boundary + crlf);
+		out.writeBytes("Content-Type: application/json" + crlf + crlf);
+		out.writeBytes(json.toString());		
+		out.writeBytes(crlf);
+		
+		out.writeBytes(twoHyphens + boundary + crlf);
+		out.writeBytes("Content-Type: application/text" + crlf + crlf);
+		BufferedReader f = new BufferedReader(new FileReader(filename));
+		
+		  String line;
+	      while((line = f.readLine()) != null){
+	          out.writeBytes(line);
+	      }
+		out.writeBytes(twoHyphens + boundary + twoHyphens + crlf);
+		out.flush();
+		out.close();
+		
+		out.close();
+		if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String decodedString;
+			String returnVal = "";
+			while ((decodedString = in.readLine()) != null) {
+				returnVal += decodedString;
+			}
+			in.close();
+			return returnVal;
+		}
+		else {
+			return "code: " + conn.getResponseCode();
+		}
+
+	}
+	
+	public static void main(String [] args)
+	{
+		SSCClient c = new SSCClient(args[0], args[1]);
+		try {
+			System.out.println(c.setHealthCheck("10", Status.OK));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
