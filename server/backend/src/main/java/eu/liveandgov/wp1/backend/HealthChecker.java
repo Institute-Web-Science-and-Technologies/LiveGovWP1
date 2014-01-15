5f0e9fe7c780e7b69770312a851b2eafd668d222
Christoph Schaefer <chrisschaefer@uni-koblenz.de>
2014-01-15 15:43:11 +0100

    Health checker coding complete

--- server/backend/src/main/java/eu/liveandgov/wp1/backend/HealthChecker.java
+++ server/backend/src/main/java/eu/liveandgov/wp1/backend/HealthChecker.java
@@ -25,14 +25,16 @@ public class HealthChecker {
 	
 	private static final String EXPECTED_SLD_RESPONSE = "{\"routes\":[{\"trip_id\":\"1074_20130930_Ti_1_1812\",\"route_id\":\"1074\",\"score\":16,\"transportation_mean\":\"Bus\",\"shape_id\":\"1074_20130812_1\"},{\"trip_id\":\"1072_20130930_Ti_2_1735\",\"route_id\":\"1072\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1072_20130812_2\"},{\"trip_id\":\"4732_20130930_Ti_2_1735\",\"route_id\":\"4732\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"4732_20130812_2\"},{\"trip_id\":\"1064_20130930_Ti_1_1808\",\"route_id\":\"1064\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1064_20110815_1\"},{\"trip_id\":\"1071_20130930_Ti_1_1805\",\"route_id\":\"1071\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1071_20121203_1\"},{\"trip_id\":\"1077_20130930_Ti_1_1808\",\"route_id\":\"1077\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1077_20130812_1\"},{\"trip_id\":\"1075_20130930_Ti_2_1738\",\"route_id\":\"1075\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1075_20130919_2\"},{\"trip_id\":\"9633_20130930_Ti_2_1717\",\"route_id\":\"9633\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"9633_20130812_2\"},{\"trip_id\":\"1067_20130930_Ti_2_1739\",\"route_id\":\"1067\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1067_20130828_2\"},{\"trip_id\":\"1067_20130930_Ti_1_1806\",\"route_id\":\"1067\",\"score\":14,\"transportation_mean\":\"Bus\",\"shape_id\":\"1067_20130828_1\"}]}";
 	private static final String LOG_DIR = "/var/log/";
-	private static final int HEALTH_CHECK_INTERVAL_IN_SEC = 180;
+	private static final int HEALTH_CHECK_INTERVAL_IN_SEC = 18;//0;
+	private static final int POST_LOG_FILES_INTERVAL_IN_SEC = 24;//*60*60;
 	static final String crlf = "\r\n";
 	static final String twoHyphens = "--";
 	static final String boundary =  "---------------------------LIVEANDGOV---";
 	
 	private String authStringBase64;
 	private int lastLineCommited = 0;
-	private String lastCommitTimestamp = "1970-01-01T00:00Z";
+	private String lastCommitTimestamp = "2013-12-06T00:00Z";
+	private String lastHealthCheckTimestamp = "2013-12-06T00:00Z";
 
 	public HealthChecker(final String user, final String pass) {
 		String authString = user + ":" + pass;
@@ -132,7 +134,7 @@ public class HealthChecker {
 	    		  continue;
 	    	  }
 	          out.writeBytes(line + "\n");
-	          lastLineCommited++;
+	          pos++;
 	      }
 	      f.close();
 		out.writeBytes( crlf + crlf + twoHyphens + boundary + twoHyphens + crlf);
@@ -142,11 +144,12 @@ public class HealthChecker {
 		if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String decodedString;
-			String returnVal = "";
+			String returnVal = "Posted " + (pos - lastLineCommited) + " lines. Cursor at position " + pos + ". Service center response: ";
 			while ((decodedString = in.readLine()) != null) {
 				returnVal += decodedString;
 			}
 			in.close();
+			lastLineCommited = pos;
 			return returnVal;
 		}
 		else {
@@ -166,39 +169,54 @@ public class HealthChecker {
 			return Status.OK;
 		}
 		else {
+			System.out.println("SLD NOK!!!");
 			return Status.CRITICAL;
 		}
 		
 	}
 	
-	class SetHealthCheckThread extends Thread {
-		HealthChecker c;
-		public SetHealthCheckThread(HealthChecker c) {
-	        this.c = c;
-	    }
-	    public void run() {
-	        for (;;) {
-	            try {
-	            	System.out.println(c.setHealthCheck("10", c.checkSLD()));
-	                sleep(HEALTH_CHECK_INTERVAL_IN_SEC * 1000);
-	            } catch (InterruptedException e) {} catch (IOException e) {
-					e.printStackTrace();
-				}
-	        }
-	    }
-	}
-	
+
 	public static void main(String [] args)
 	{
-		HealthChecker c = new HealthChecker(args[0], args[1]);
-		try {
-			System.out.println(c.setHealthCheck("10", c.checkSLD()));
-//			System.out.println(c.postLogFile("13", "ServiceLineDetection.log"));
-			System.out.println(c.checkSLD());
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// simple thread wrapper for health checks
+		class SetHealthCheckThread extends Thread {
+			HealthChecker c;
+			public SetHealthCheckThread(HealthChecker c) {
+		        this.c = c;
+		    }
+		    public void run() {
+		        for (;;) {
+		            try {
+		            	c.lastHealthCheckTimestamp = c.getISO8601UTCDate(new Date());
+		            	System.out.println(c.lastHealthCheckTimestamp + " Health check. Service center response: " + c.setHealthCheck("10", c.checkSLD()));
+		                sleep(HEALTH_CHECK_INTERVAL_IN_SEC * 1000);
+		            } catch (InterruptedException e) {} catch (IOException e) {
+						e.printStackTrace();
+					}
+		        }
+		    }
 		}
+		
+		// simple thread wrapper for log files
+		class PostLogFileThread extends Thread {
+			HealthChecker c;
+			public PostLogFileThread(HealthChecker c) {
+		        this.c = c;
+		    }
+		    public void run() {
+		        for (;;) {
+		            try {
+		            	System.out.println(c.lastCommitTimestamp + " Log file post. " + c.postLogFile("13", "ServiceLineDetection.log"));
+		                sleep(POST_LOG_FILES_INTERVAL_IN_SEC * 1000);
+		            } catch (InterruptedException e) {} catch (IOException e) {
+						e.printStackTrace();
+					}
+		        }
+		    }
+		}
+		
+		HealthChecker c = new HealthChecker(args[0], args[1]);
+		new SetHealthCheckThread(c).start();
+		new PostLogFileThread(c).start();
 	}
 }
