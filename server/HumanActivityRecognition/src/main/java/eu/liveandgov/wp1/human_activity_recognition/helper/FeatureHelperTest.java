002152ea0af52158e1541574acee51ef48b5439b
Heinrich Hartmann <hartmann@uni-koblenz.de>
2014-01-28 17:01:39 +0100

    Added FFT Feature

---
+++ server/HumanActivityRecognition/src/main/java/eu/liveandgov/wp1/human_activity_recognition/helper/FeatureHelperTest.java
@@ -0,0 +1,13 @@
+package eu.liveandgov.wp1.human_activity_recognition.helper;
+
+import org.junit.Test;
+
+/**
+ * Created by hartmann on 1/28/14.
+ */
+public class FeatureHelperTest {
+    @Test
+    public void testPadZero() throws Exception {
+
+    }
+}
