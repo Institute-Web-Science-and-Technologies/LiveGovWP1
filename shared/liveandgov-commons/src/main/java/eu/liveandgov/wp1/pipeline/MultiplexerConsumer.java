0e93075f655cc250a238cfd95ad69e507b8e091b
Lukas Härtel <lukashaertel@googlemail.com>
2014-02-11 18:17:10 +0100

    Filled in some info for Maven

--- shared/liveandgov-commons/src/main/java/eu/liveandgov/wp1/pipeline/MultiplexerConsumer.java
+++ shared/liveandgov-commons/src/main/java/eu/liveandgov/wp1/pipeline/MultiplexerConsumer.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * <p>Replaces the old MultiProducer</p>
+ * <p>This pipeline element provides a mutable consumer-set. On receiving an item on its input, it distributes
+ * it to all consumers in this set.</p>
  * Created by Lukas Härtel on 10.02.14.
  */
 public class MultiplexerConsumer<Item> implements Consumer<Item> {
@@ -19,7 +22,7 @@ public class MultiplexerConsumer<Item> implements Consumer<Item> {
 
     @Override
     public void push(Item item) {
-        for(Consumer<? super Item> consumer : consumers){
+        for (Consumer<? super Item> consumer : consumers) {
             consumer.push(item);
         }
     }
