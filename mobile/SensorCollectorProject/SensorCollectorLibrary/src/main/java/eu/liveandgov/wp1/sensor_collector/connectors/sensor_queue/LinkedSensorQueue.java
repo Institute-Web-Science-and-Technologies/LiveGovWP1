066b2957bcc53a4b13b68592a6e7abaa40d412b1
Lukas Härtel <lukashaertel@googlemail.com>
2014-03-01 11:16:58 +0100

    Adapted the new changes from Platform Proximity, i.e. settings-window and dynamic ZMQ destination selection. Fixed a bug in the queue that fails because of a side effect being executed even if not desired.

--- mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/connectors/sensor_queue/LinkedSensorQueue.java
+++ mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/connectors/sensor_queue/LinkedSensorQueue.java
@@ -7,34 +7,40 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * Simple queue class of a fixed maximal capacity.
  * If the capacity is reached further messages are dropped.
  * The class provides a blockinPull() method, that blocks until new messages are available.
- *
+ * <p/>
  * Created by hartmann on 9/29/13.
  */
 public class LinkedSensorQueue implements SensorQueue {
-    public  static final int capacity = 1000;
+    public static final int capacity = 1000;
 
     private int size = 0;
-    public  final Queue<String> Q = new ConcurrentLinkedQueue<String>();
+    public final Queue<String> Q = new ConcurrentLinkedQueue<String>();
 
     /**
      * Push message to the queue.
      * Drop message if queue is full.
+     *
      * @param m
      */
     @Override
-    public void push(String m){
-        if (size++ < capacity) {
-            Q.add(m);
+    public void push(String m) {
+        if (size + 1 < capacity) {
+            size++;
+            Q.offer(m);
         }
     }
 
-    private String pull(){
-        size = Math.max(size - 1, 0);
-        return Q.poll();
+    private String pull() {
+        if (size > 0) {
+            size--;
+            return Q.poll();
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public String blockingPull(){
+    public String blockingPull() {
         String m;
 
         while (true) {
--- mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/streaming/ZMQStreamer.java
+++ mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/streaming/ZMQStreamer.java
@@ -1,19 +1,22 @@
 package eu.liveandgov.wp1.sensor_collector.streaming;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import org.jeromq.ZContext;
 import org.jeromq.ZMQ;
 
 import eu.liveandgov.wp1.human_activity_recognition.connectors.Consumer;
+import eu.liveandgov.wp1.sensor_collector.GlobalContext;
+import eu.liveandgov.wp1.sensor_collector.R;
 import eu.liveandgov.wp1.sensor_collector.configuration.SensorCollectionOptions;
 import eu.liveandgov.wp1.sensor_collector.monitor.Monitorable;
 
 /**
-* String-Consumer that sends samples to a remote server using ZMQ message queue system.
-*
-* Created by hartmann on 10/2/13.
-*/
+ * String-Consumer that sends samples to a remote server using ZMQ message queue system.
+ * <p/>
+ * Created by hartmann on 10/2/13.
+ */
 public class ZMQStreamer implements Monitorable, Consumer<String> {
     public final static String LOG_TAG = "ZMQStreamer";
 
@@ -23,32 +26,50 @@ public class ZMQStreamer implements Monitorable, Consumer<String> {
 
     private ZMQ.Socket socket;
 
-    public ZMQStreamer(){
+    public ZMQStreamer() {
         context = new ZContext();
         context.setHWM(1000);
     }
 
+    private static String getAddress() {
+        final SharedPreferences settings = GlobalContext.context.getSharedPreferences(GlobalContext.context.getString(R.string.spn), 0);
+
+        final String streamingAddressValue = settings.getString(GlobalContext.context.getString(R.string.prf_streaming_address), null);
+
+        if (streamingAddressValue == null)
+            return SensorCollectionOptions.STREAMING_ZMQ_SOCKET;
+        else {
+            if (streamingAddressValue.matches("[^:]+:\\d+"))
+                return "tcp://" + streamingAddressValue;
+            else
+                return "tcp://" + streamingAddressValue + ":5555";
+        }
+    }
+
+    private String lcon = null;
+
     @Override
-    public void push(String m)
-    {
-        for(int i=0;i<MESSAGE_RETRY_COUNT;i++)
-        {
-            if(socket == null)
-            {
+    public void push(String m) {
+        for (int i = 0; i < MESSAGE_RETRY_COUNT; i++) {
+            if (socket == null) {
                 socket = context.createSocket(ZMQ.PUSH);
-                socket.connect(SensorCollectionOptions.STREAMING_ZMQ_SOCKET);
+                socket.connect(lcon = getAddress());
+            } else {
+                String ncon = getAddress();
+
+                if (!lcon.equals(ncon)) {
+                    socket.connect(lcon = ncon);
+                }
             }
 
-            if(!socket.send(m, ZMQ.DONTWAIT))
-            {
+            if (!socket.send(m + "\r\n", ZMQ.DONTWAIT)) {
                 Log.w(LOG_TAG, "ZMQ connection transmission failed, invalidating socket");
 
                 context.destroySocket(socket);
                 socket = null;
             }
 
-            if(socket != null)
-            {
+            if (socket != null) {
                 return;
             }
         }
