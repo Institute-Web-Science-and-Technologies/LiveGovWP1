91d58a3bec2dde6bff30faf839b41ac333452ee6
Lukas Härtel <lukashaertel@googlemail.com>
2014-03-02 16:13:23 +0100

    Replaced scheduling and suspend/resume pattern with a blocking queue pattern similar to the connector thread and sensor queue approach

--- shared/liveandgov-commons/src/main/java/eu/liveandgov/wp1/pipeline/impl/Player.java
+++ shared/liveandgov-commons/src/main/java/eu/liveandgov/wp1/pipeline/impl/Player.java
@@ -13,7 +13,7 @@ public class Player<Item> extends Pipeline<Item, Item> implements Stoppable {
     /**
      * The service executing the playback
      */
-    public final ScheduledExecutorService scheduledExecutorService;
+    public final ExecutorService executorService;
 
     /**
      * The limit of items to store in the queue or -1 for no limit
@@ -21,86 +21,48 @@ public class Player<Item> extends Pipeline<Item, Item> implements Stoppable {
     public final int limit;
 
     /**
-     * The limit of items simultaneously played back or -1 to play all queued
-     */
-    public final int playbackLimit;
-
-    /**
-     * The rate of playback
-     */
-    public final long playbackRate;
-
-    /**
-     * Event called with false when playback is suspended
-     */
-    public final CallbackSet<Boolean> suspended = new CallbackSet<Boolean>();
-
-    /**
-     * Event called with true when playback is resumed
-     */
-    public final CallbackSet<Boolean> resumed = new CallbackSet<Boolean>();
-
-    /**
      * All items of this player
      */
-    private final ConcurrentLinkedQueue<Item> queue;
+    private final BlockingQueue<Item> queue;
 
     /**
      * The task handling the playback
      */
-    private ScheduledFuture<?> task;
+    private final Future<?> task;
 
     /**
      * Creates a new player with the given parameters
      *
-     * @param scheduledExecutorService The executor service used to schedule the playback
-     * @param limit                    The limit of items to store in the queue or -1 for no limit
-     * @param playbackLimit            The limit of items simultaneously played back or -1 to play all queued
-     * @param playbackRate             The rate of playback
+     * @param executorService The executor service used to run the playback
+     * @param limit           The limit of items to store in the queue or -1 for no limit
      */
-    public Player(ScheduledExecutorService scheduledExecutorService, int limit, int playbackLimit, long playbackRate) {
-        this.scheduledExecutorService = scheduledExecutorService;
+    public Player(ExecutorService executorService, int limit) {
+        this.executorService = executorService;
         this.limit = limit;
-        this.playbackLimit = playbackLimit;
-        this.playbackRate = playbackRate;
 
-        queue = new ConcurrentLinkedQueue<Item>();
-    }
-
-    private void resume() {
-        task = scheduledExecutorService.scheduleWithFixedDelay(playerJob, 0L, playbackRate, TimeUnit.MILLISECONDS);
-
-        resumed.call(true);
-    }
-
-    private void suspend() {
-        task.cancel(true);
-        task = null;
-
-        suspended.call(false);
+        queue = new LinkedBlockingQueue<Item>();
+        task = executorService.submit(playerJob);
     }
 
     @Override
     public void push(final Item item) {
-        if (queue.size() + 1 <= limit || limit == -1) {
-            queue.offer(item);
-        }
-
-        if (task == null) {
-            resume();
-        }
+        if (limit == -1 || queue.size() < limit)
+            try {
+                queue.put(item);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
     }
 
     private final Runnable playerJob = new Runnable() {
         @Override
         public void run() {
-            for (int i = 0; !queue.isEmpty() && (i < playbackLimit || playbackLimit == -1); i++) {
-                produce(queue.poll());
-            }
-
-            // If the queue is empty, stop this task if the queue has no more elements
-            if (queue.isEmpty()) {
-                suspend();
+            while (true) {
+                try {
+                    produce(queue.take());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
             }
         }
     };
@@ -108,6 +70,6 @@ public class Player<Item> extends Pipeline<Item, Item> implements Stoppable {
 
     @Override
     public void stop() {
-        suspend();
+        task.cancel(true);
     }
 }
