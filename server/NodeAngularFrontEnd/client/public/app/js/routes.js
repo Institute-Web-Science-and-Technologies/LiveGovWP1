2f190e62dde76858a3d764b3826e88112d43a8bf
René Wilhelm <rene.wilhelm@gmail.com>
2014-03-03 01:50:02 +0100

    Minor cleanups

--- server/NodeAngularFrontEnd/client/public/app/js/routes.js
+++ server/NodeAngularFrontEnd/client/public/app/js/routes.js
@@ -1,10 +1,13 @@
+/* jshint -W097 */
 'use strict';
+
+/* global app:true, console:true, confirm:true */
 
 /* ROUTES */
 
-app.config(function($routeProvider, $locationProvider) {
-  // $locationProvider.html5Mode(true);
-  // $locationProvider.hashPrefix('!');
+app.config(function ($routeProvider, $locationProvider) {
+	// $locationProvider.html5Mode(true);
+	// $locationProvider.hashPrefix('!');
 
 	$routeProvider
 		.when('/rec', {
@@ -13,35 +16,37 @@ app.config(function($routeProvider, $locationProvider) {
 			// reloadOnSearch: false
 		})
 
-		.when('/rec/:trip_id', {
-			templateUrl: '/partials/rec',
-			controller: 'recCtrl'
-		})
+	.when('/rec/:trip_id', {
+		templateUrl: '/partials/rec',
+		controller: 'recCtrl'
+	})
 
-		.when('/raw', {
-			templateUrl: '/partials/raw',
-			controller: 'rawCtrl'
-		})
+	.when('/raw', {
+		templateUrl: '/partials/raw',
+		controller: 'rawCtrl'
+	})
 
-		.when('/raw/:trip_id', {
-			templateUrl: '/partials/raw',
-			controller: 'rawCtrl'
-		})
+	.when('/raw/:trip_id', {
+		templateUrl: '/partials/raw',
+		controller: 'rawCtrl'
+	})
 
-		.when('/har', {
-			templateUrl: '/partials/har',
-			controller: 'harCtrl'
-		})
+	.when('/har', {
+		templateUrl: '/partials/har',
+		controller: 'harCtrl'
+	})
 
-		.when('/har/:trip_id', {
-			templateUrl: '/partials/har',
-			controller: 'harCtrl'
-		})
+	.when('/har/:trip_id', {
+		templateUrl: '/partials/har',
+		controller: 'harCtrl'
+	})
 
-		.when('/sld', {
-			templateUrl: '/partials/sld',
-			controller: 'sldCtrl'
-		})
+	.when('/sld', {
+		templateUrl: '/partials/sld',
+		controller: 'sldCtrl'
+	})
 
-		.otherwise({ redirectTo: '/rec' })
+	.otherwise({
+		redirectTo: '/rec'
+	});
 });
