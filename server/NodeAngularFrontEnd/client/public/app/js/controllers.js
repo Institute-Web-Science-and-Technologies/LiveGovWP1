ea2481a5a3bd6c012d23dce4a0997140c91f64c9
René Wilhelm <rene.wilhelm@gmail.com>
2014-03-07 11:06:55 +0100

    Rewrote controller and service to use promises

--- server/NodeAngularFrontEnd/client/public/app/js/controllers.js
+++ server/NodeAngularFrontEnd/client/public/app/js/controllers.js
@@ -2,12 +2,10 @@
 /* jshint -W097 */
 'use strict';
 
-// CHANGEME DRY!
-
 /* CONTROLLERS */
-
+var a;
 // records tab
-app.controller('recCtrl', function ($scope, $rootScope, $routeParams, $location, Data, Trip, Sensor, Map) { // sensorData
+app.controller('recCtrl', function ($scope, $rootScope, $routeParams, $location, Data, Trip, Sensor, Map) {
 
 	// initialize our shared data scope
 	$scope.data = Data;
@@ -39,136 +37,88 @@ app.controller('recCtrl', function ($scope, $rootScope, $routeParams, $location,
 	};
 
 	// click on a trip to globally select it
-    this.select = function (trip) {
+	this.select = function (trip) {
 		$rootScope.trip = trip;
 
-
-		// $rootScope.fuck = sensorData(trip.trip_id);
-		// console.log($rootScope.fuck);
-
+		// reset brush selection if trip changed
+		$scope.data.selection = '';
 
 		$routeParams.trip_id = trip.trip_id; // FIXME rewrite and react on $locationParams (if possible)
 	};
 
-	// highlight the selected trip by changing css class to 'selected'
+	// highlight the selected trip (see rec.jade)
 	this.is = function(trip) {
 		return ($rootScope.trip && $rootScope.trip.trip_id === trip.trip_id) ? true : false;
 	};
 
-	// watch the shared scope for our selected trip and start getting all
-	// required data to reduce waiting time when switching to another view
 	$rootScope.$watch(
-		// listener: wait for $rootScope.trip changes
 		function () {
 			return $rootScope.trip;
 		},
-		// change handler: do this when $rootScope.trip changed
 		function (newTrip, oldTrip) {
 			if (newTrip !== oldTrip) {
 				if ($rootScope.trip) {
 					console.log("TRIP HAS CHANGED. LOADING NEW DATA.");
-					$scope.data.acc = Sensor.acc({trip_id: $rootScope.trip.trip_id });
-					$scope.data.lac = Sensor.lac({trip_id: $rootScope.trip.trip_id });
-					$scope.data.gra = Sensor.gra({trip_id: $rootScope.trip.trip_id });
+
 					$scope.data.gps = Map.gps({trip_id: $rootScope.trip.trip_id });
 					$scope.data.har = Map.har({trip_id: $rootScope.trip.trip_id });
+
+					var loadData = function (trip_id, sensor) {
+						Sensor.query({
+							trip_id: trip_id,
+							sensor: sensor
+						}).then(function (data) {
+							console.log('Success: ' + sensor + ' data for trip ' + $rootScope.trip.trip_id + ' returned');
+							$scope.data[sensor] = data;
+						}, function (data) {
+							console.log("Failure: No " + sensor + " data for trip " + $rootScope.trip.trip_id + " returned");
+						});
+					};
+
+					loadData($rootScope.trip.trip_id, 'gra');
+					loadData($rootScope.trip.trip_id, 'acc');
+					loadData($rootScope.trip.trip_id, 'lac');
+
 				}
 			}
 		}
 	);
-
 });
 
 // raw
 /* jshint -W030 */
-app.controller('rawCtrl', function ($scope, $rootScope, $location, Sensor, Data, debounce) {
+app.controller('rawCtrl', function ($scope, $rootScope, $location, Sensor, Data) {
 
-	// redirect to /rec if there's no trip selected
 	$rootScope.trip || $location.path('/rec');
-
-	// put shared data object into scope
 	$scope.data = Data;
 
-	// helper to get closest starttime of interpolated ts of sensor data
-	var starttime = function(ts, data) {
-		for (var i=0; i<data.length; i++) {
-			if (data[i].starttime >= +ts) {
-				return [i, data[i].starttime];
+	$scope.$watchCollection('data.selection', function (sel, oldSel) {
+		if (sel !== oldSel) {
+			console.log("BRUSH DETECTED: " + sel);
+
+			var loadMoreData = function(trip_id, sensor, sel) {
+				console.log("Old data length: " + $scope.data[sensor].length);
+				Sensor.query({
+					trip_id: trip_id,
+					sensor: sensor,
+					sel: sel
+				}).then(function (data) {
+					console.log('Success: ' + sensor + ' data for trip ' + $rootScope.trip.trip_id + ' returned');
+					console.log(data);
+					console.log("New data length: " + data.length);
+					$scope.data[sensor] = data;
+				}, function (data) {
+					console.log("Failure: No " + sensor + " data for trip " + $rootScope.trip.trip_id + " returned");
+				});
+			};
+
+			if ($rootScope.trip) {
+				loadMoreData($rootScope.trip.trip_id, 'gra', sel);
+				loadMoreData($rootScope.trip.trip_id, 'acc', sel);
+				loadMoreData($rootScope.trip.trip_id, 'lac', sel);
 			}
 		}
-	};
-
-	// helper to get closest endtime of interpolated ts of sensor data
-	var endtime = function(ts, data) {
-		for (var i=data.length-1; i>0; i--) {
-			if (data[i].endtime <= +ts) {
-				return [i, data[i].endtime];
-			}
-		}
-	};
-
-	// sort data by timestamp (do we really need that?)
-	var sortData = function(data) {
-		data.sort(function(a, b) {
-			return ((a.ts < b.ts) ? -1 : ((b.ts > a.ts) ? 1 : 0));
-		});
-	};
-
-	// TODO
-	// 1. after data is loaded, compute default (unbrushed) selection range
-	// 2. watch $scope.selection for changes (via brush directive)
-	// 3. if selection is changed, chart directive should react by zooming functions
-
-	$scope.$watchCollection('data.selection', debounce(function (newVal, oldVal) {
-		if (newVal !== oldVal) {
-			console.log("RAW:   " + newVal);
-
-			// TODO
-
-			var start = starttime(newVal[0], $scope.data.acc);
-			var end = endtime(newVal[1], $scope.data.acc);
-
-			// load more data for a specific sensor
-			Sensor.acc({
-				trip_id: $rootScope.trip.trip_id,
-				startTime: start[1],
-				endTime: end[1]
-			}).$promise.then(function(data) {
-				// merge old and new data and remove obsolete values
-				// $scope.data.acc.splice(start, end-start, data);
-				var args = [start[0]+1, end[0]-start[0]].concat(data);
-				console.log(args);
-				Array.prototype.splice.apply($scope.data.acc, args));
-				// sortData(Array.prototype.splice.apply($scope.data.acc, args));
-
-				console.log('PUSHED NEW DATA ' + $scope.data.acc.length);
-				console.log($scope.data.acc);
-			});
-
-			Sensor.lac({
-				trip_id: $rootScope.trip.trip_id,
-				startTime: start,
-				endTime: end
-			}).$promise.then(function(data) {
-				var args = [start, end-start].concat(data);
-				Array.prototype.splice.apply($scope.data.lac, args);
-				console.log('PUSHED NEW DATA ' + $scope.data.lac.length);
-				// console.log($scope.data.lac);
-			});
-
-			Sensor.gra({
-				trip_id: $rootScope.trip.trip_id,
-				startTime: start,w
-				endTime: end
-			}).$promise.then(function(data) {
-				var args = [start, end-start].concat(data);
-				Array.prototype.splice.apply($scope.data.gra, args);
-				console.log('PUSHED NEW DATA ' + $scope.data.gra.length);
-				// console.log($scope.data.gra);
-			});
-		}
-	}, 1000));
-
+	});
 });
 
 // har
@@ -185,5 +135,4 @@ app.controller('navCtrl', function ($scope, $route, Data) {
 	this.is = function(loc) {
 		return ($route.current && $route.current.name == loc) ? true : false;
 	};
-
 });
--- server/NodeAngularFrontEnd/client/public/app/js/services.js
+++ server/NodeAngularFrontEnd/client/public/app/js/services.js
@@ -1,28 +1,32 @@
 /* jshint -W097 */
-/* global app:true, console:true, angular:true */
+/* global app:true, console:true, angular:true, d3:true */
 
 'use strict';
 
+
+
+
+
 app.factory('Data', function () {
 	// return a at first empty object. will be populated.
-	return {};
+	return {
+		'gra': [],
+		'acc': [],
+		'lac': []
+	};
 });
+
+
+
+
 
 app.factory('Trip', function($resource) {
 	return $resource('/trips/:trip_id', { trip_id: '@trip_id' });
 });
 
-app.factory('Sensor', function($resource) {
-  return $resource(
-    '/trips/:trip_id/:sensor/window', {trip_id: '@trip_id' }, {
-      acc: {method: 'GET', 'params': {sensor: 'acc'}, isArray: true},
-      lac: {method: 'GET', 'params': {sensor: 'lac'}, isArray: true},
-      gra: {method: 'GET', 'params': {sensor: 'gra'}, isArray: true}
-    }
-  );
-});
 
-// TODO rewrite using $http, so we can get data at once and prepare it
+
+
 
 app.factory('Map', function($resource) {
 	return $resource(
@@ -32,6 +36,117 @@ app.factory('Map', function($resource) {
 		}
 	);
 });
+
+
+
+
+
+app.factory('Sensor', ['$http', '$q', 'Data', function ($http, $q, Data) {
+
+// get closest starttime index of interpolated ts of sensor data
+	var startIdx = function(ts, data) {
+		for (var i=0; i<data.length; i++) {
+			if (data[i].starttime >= +ts) {
+				return i;
+			}
+		}
+	};
+
+	// get closest endtime index of interpolated ts of sensor data
+	var endIdx = function(ts, data) {
+		for (var i=data.length-1; i>0; i--) {
+			if (data[i].endtime <= +ts) {
+				return i;
+			}
+		}
+	};
+
+	var sortData = function(data) {
+		// data.sort(function(a, b) { return d3.ascending(a.starttime, b.starttime); });
+		data.sort(function(a, b) {
+			return +a.ts < +b.ts ? -1 : +a.ts > +b.ts ? 1 : 0;
+		});
+	};
+
+	var factory = {
+		query: function(value) {
+
+			if (!value.sensor)   { console.log("Error: sensor missing");  return; }
+			if (!value.trip_id)  { console.log("Error: trip_id missing"); return; }
+
+			var trip_id = value.trip_id,
+					sensor = value.sensor,
+					oldData = Data[sensor],
+					startTime = '',
+					endTime = '',
+					windowSize = '';
+
+			if (value.sel) {
+				startTime = "?starttime=" + value.sel[0].getTime();
+				endTime   = "?endtime="   + value.sel[1].getTime();
+			}
+
+			if (value.windowSize) {
+				windowSize = "?window=" + value.windowSize;
+			}
+
+			var deferred = $q.defer();
+
+			console.log('asking for ' + sensor + ' data of trip ' + trip_id);
+			var queryStartTime = new Date();
+
+			$http({ method: "GET", url: '/trips/' + trip_id + '/' + sensor + '/window' + startTime + endTime + windowSize })
+				.success(function (data, status, headers, config) {
+					console.log("Success: received " + sensor + " data for trip " + trip_id + " after " + ((new Date() - queryStartTime) / 1000) + " ms");
+
+					data.forEach(function(d) {
+						d.ts         = new Date((+d.starttime + +d.endtime) / 2);
+						d.starttime  = +d.starttime;
+						d.endtime    = +d.endtime;
+					});
+
+					if (oldData.length > 0) {
+
+						var start = startIdx(startTime, oldData);
+						var range = endIdx(endTime, oldData) - start;
+
+						// Array.prototype.splice.apply(oldData, [start, range].concat(data));
+						console.log("VVVoldData" + oldData.length);
+						console.log("WWWdata   " + data.length);
+						var args = [start, range].concat(data);
+						console.log(Array.prototype.splice.apply(oldData, args));
+						Array.prototype.splice.apply(oldData, args);
+						data = sortData(oldData);
+
+						// console.log(oldData.length);
+						// console.log(oldData);
+						// console.log(data.length);
+						// console.log(data);
+
+						console.log("Success: merged " + sensor + " data for trip " + trip_id + " after " + ((new Date() - queryStartTime) / 1000) + " ms");
+					}
+
+					// for (var i=1; i<a.length; i++) { a[i-1].starttime > a[i].starttime ? console.log(a[i-1].starttime + " " + a[i].starttime + " " + i) : ''; }
+
+					console.log("Success: prepared " + sensor + " data for trip " + trip_id + " after " + ((new Date() - queryStartTime) / 1000) + " ms");
+					console.log("YYY");
+					console.log(data); // FIXME undefined
+					deferred.resolve(data);
+					console.log("Success: serving " + sensor + " data for trip " + trip_id + " after " + ((new Date() - queryStartTime) / 1000) + " ms");
+				}).error(function (data, status, headers, config) {
+					console.log("Failure: couldn't get any " + sensor + " data for trip " + trip_id);
+					deferred.reject(data);
+				});
+
+			return deferred.promise;
+		}
+	};
+	return factory;
+}]);
+
+
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // DEBOUNCE SERVICE
@@ -46,28 +161,28 @@ app.factory('Map', function($resource) {
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
 app.factory('debounce', function($timeout, $q) {
-  return function(func, wait, immediate) {
-    var timeout;
-    var deferred = $q.defer();
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if(!immediate) {
-          deferred.resolve(func.apply(context, args));
-          deferred = $q.defer();
-        }
-      };
-      var callNow = immediate && !timeout;
-      if ( timeout ) {
-        $timeout.cancel(timeout);
-      }
-      timeout = $timeout(later, wait);
-      if (callNow) {
-        deferred.resolve(func.apply(context,args));
-        deferred = $q.defer();
-      }
-      return deferred.promise;
-    };
-  };
+	return function(func, wait, immediate) {
+		var timeout;
+		var deferred = $q.defer();
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if(!immediate) {
+					deferred.resolve(func.apply(context, args));
+					deferred = $q.defer();
+				}
+			};
+			var callNow = immediate && !timeout;
+			if ( timeout ) {
+				$timeout.cancel(timeout);
+			}
+			timeout = $timeout(later, wait);
+			if (callNow) {
+				deferred.resolve(func.apply(context,args));
+				deferred = $q.defer();
+			}
+			return deferred.promise;
+		};
+	};
 });
