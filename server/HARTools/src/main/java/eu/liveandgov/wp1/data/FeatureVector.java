18ad1cbb89e25049fb9d2623e665a2aab4b35e6b
Heinrich Hartmann <hartmann@uni-koblenz.de>
2014-03-07 14:50:39 +0100

    Changed feature configuration.
    Removed energy

--- server/HARTools/src/main/java/eu/liveandgov/wp1/data/FeatureVector.java
+++ server/HARTools/src/main/java/eu/liveandgov/wp1/data/FeatureVector.java
@@ -1,17 +1,16 @@
 package eu.liveandgov.wp1.data;
 
-import eu.liveandgov.wp1.data.Window;
 import eu.liveandgov.wp1.helper.BinDistributor;
 import eu.liveandgov.wp1.helper.FeatureHelper;
 import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * Created by cehlen on 10/19/13.
  */
 public class FeatureVector {
-
-    private final int S2_BINS_NUM = 10;
-    private final int S2_FT_BINS_NUM = 5;
 
     public String tag = "";
     public String id = "";
@@ -30,7 +29,7 @@ public class FeatureVector {
 
     // Tilting Angle
     public float tilt = 0F;
-    public float energy = 0F;
+    // public float energy = 0F;
     public float kurtosis = 0F;
 
     public Integer [] S2Bins = new Integer[0];
@@ -59,13 +58,13 @@ public class FeatureVector {
         s2Var = FeatureHelper.var(S2);
 
         tilt = FeatureHelper.tilt(xMean, yMean, zMean);
-        energy = FeatureHelper.sum(S2);
+        // energy = FeatureHelper.sum(S2); this is the same as s2Mean
         kurtosis = FeatureHelper.kurtosis(S2);
 
-        BinDistributor BD = new BinDistributor(0,20,S2_BINS_NUM);
-        S2Bins = BD.getBinsForAxis(S2);
+        // BinDistributor BD = new BinDistributor(0,20,10);
+        // S2Bins = BD.getBinsForAxis(S2);
 
-        BinDistributor FBD = new BinDistributor(0,100,S2_FT_BINS_NUM);
+        BinDistributor FBD = new BinDistributor(0,100,5);
         S2FTBins = FBD.getBinsForAxis(FeatureHelper.FTAbsolute(S2));
 
         startTime = m.startTime;
@@ -73,8 +72,10 @@ public class FeatureVector {
 
 
     public String toCSV() {
-        String out = String.format("%s,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f",
-                id, tag, xMean, yMean, zMean, xVar, yVar, zVar, s2Mean, s2Var, tilt, energy, kurtosis
+        String out = String.format("%s,%s,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f",
+                id, tag, xMean, yMean, zMean, xVar, yVar, zVar, s2Mean, s2Var, tilt,
+                // energy,
+                kurtosis
         );
 
 //        out += "," + StringUtils.join(S2Bins, ',');
@@ -90,28 +91,30 @@ public class FeatureVector {
 
     // Needed for classification
     public Object[] toWekaObjArr() {
-        int size = 11 + S2_FT_BINS_NUM + 2;
-        Object r[] = new Object[size];
-        r[0] = (double)xMean;
-        // Currently we are using the SD because our first tree was using it
-        r[1] = (double)yMean; // THIS NEEDS TO BE SQRT FOR THE CRAPPY CLASSIFIER
-        r[2] = (double)zMean;
-        r[3] = (double)xVar;
-        r[4] = (double)yVar;
-        r[5] = (double)zVar;
-        r[6] = (double)s2Mean;
-        r[7] = (double)s2Var;
-        r[8] = (double)tilt;
-        r[9] = (double)energy;
-        r[10] = (double)kurtosis;
-//        for(int i = 0; i < S2_BINS_NUM+2; i++) {
-//            r[i+10] = (double)S2Bins[i];
+
+        Deque<Object> R = new ArrayDeque<Object>();
+
+        R.addLast((double)xMean);
+        R.addLast((double)yMean);
+        R.addLast((double)zMean);
+        R.addLast((double)xVar);
+        R.addLast((double)yVar);
+        R.addLast((double)zVar);
+        R.addLast((double)s2Mean);
+        R.addLast((double)s2Var);
+        R.addLast((double)tilt);
+//        R.addLast((double)energy);
+        R.addLast((double)kurtosis);
+
+//        for (float f : S2Bins){
+//            R.addLast((double) f);
 //        }
-        for(int i = 0; i < S2_FT_BINS_NUM+2; i++) {
-            r[i+S2_BINS_NUM] = (double)S2FTBins[i];
+
+        for (float f : S2FTBins){
+            R.addLast((double) f);
         }
 
-        return r;
+        return R.toArray();
 
     }
 
