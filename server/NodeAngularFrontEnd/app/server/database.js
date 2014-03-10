d81dc1aaebd1fc51599eb7827ba9dceb7e572376
René Wilhelm <rene.wilhelm@gmail.com>
2014-03-10 14:07:52 +0100

    Fixes and cleanups

--- server/NodeAngularFrontEnd/app/server/database.js
+++ server/NodeAngularFrontEnd/app/server/database.js
@@ -7,25 +7,7 @@ module.exports = {
 			client.query(statement, function(err, result) {
 				done();
 				if (err) { console.error(err); return; }
-
-				console.log(statement);
-
-				// if (statement.name === "showTrip" || statement.name === "allTrips" ) {
-				// 	result.rows.map(function(row) {
-				// 		if (row.stop_ts >= row.start_ts) {
-				// 			row.duration = parseInt(row.stop_ts) - parseInt(row.start_ts) - 3600000;
-				// 		} else {
-				// 			row.duration = parseInt(row.stop_ts) - parseInt(row.start_ts);
-				// 			console.log("WARNING: BROKEN TIMESTAMP: " + row.trip_id);
-				// 		}
-				// 	});
-				// }
-
-				if (statement.name === "showTrip" || statement.name === "allTrips" ) {
-					result.rows.map(function(row) {
-							row.duration = parseInt(row.stop_ts) - parseInt(row.start_ts) - 3600000;
-					});
-				}
+				console.log('\x1b[35m', JSON.stringify(statement, null, 2), '\x1b[0m'); // magenta
 				callback(err, result.rows);
 			});
 		});
@@ -36,7 +18,7 @@ module.exports = {
 		var statement = {
 			name: "allTrips",
 			text: text
-		}
+		};
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
@@ -45,11 +27,13 @@ module.exports = {
 	},
 
 	show: function(params, query, callback) {
+		var statement;
+
 		if (params.uuid !== undefined) {
-			var statement = {
+			statement = {
 				text: "SELECT * FROM trip WHERE user_id = $1",
 				values: [params.uuid]
-			}
+			};
 		} else if (params.sensor !== undefined) {
 			switch (params.sensor) {
 				case "acc": sensor = "sensor_accelerometer"; break;
@@ -60,17 +44,17 @@ module.exports = {
 				case "gra": sensor = "sensor_gravity"; break;
 				case "tag": sensor = "sensor_tags"; break;
 			}
-			var statement = {
+			statement = {
 				text: "SELECT * FROM " + sensor + " WHERE trip_id = $1",
 				// text: "SELECT ts, x, y, z FROM " + sensor + " WHERE trip_id = $1",
 				values: [params.trip_id]
-			}
+			};
 		} else {
-			var statement = {
+			statement = {
 				name: "showTrip",
 				text: "SELECT * FROM trip WHERE trip_id = $1",
 				values: [params.trip_id]
-			}
+			};
 		}
 
 		this.query(statement, function(err, result) {
@@ -83,7 +67,7 @@ module.exports = {
 		var statement = {
 			text: "SELECT ts, ST_AsGeoJSON(lonlat)::json AS lonlat from sensor_gps WHERE trip_id = $1",
 			values: [params.trip_id]
-		}
+		};
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
@@ -93,9 +77,9 @@ module.exports = {
 
 	update: function(params, body, callback) {
 		var statement = {
-			text: "UPDATE trip SET name = $2 WHERE trip_id = $1", // FIXME
+			text: "UPDATE trip SET name = $2 WHERE trip_id = $1",
 			values: [params.trip_id, body.name]
-		}
+		};
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
@@ -107,7 +91,7 @@ module.exports = {
 		var statement = {
 			text: "DELETE FROM trip WHERE trip_id = $1",
 			values: [params.trip_id]
-		}
+		};
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
@@ -122,10 +106,9 @@ module.exports = {
 			case "gra": sensor = "sensor_gravity"; break;
 		}
 
-		var text = "SELECT avg(x) AS avgX, min(x) AS minX, max(x) AS maxX, avg(y) AS avgY, min(y) AS minY, max(y) AS maxY, avg(z) AS avgZ, min(z) AS minZ, max(z) AS maxZ, min(ts) AS startTime, max(ts) AS endTime FROM (SELECT x, y, z, ts, NTILE($2) OVER (ORDER BY ts) AS w FROM " + sensor + " WHERE trip_id = $1{{LIMIT_TIME}}) A GROUP BY w ORDER BY w;"
+		var text = "SELECT avg(x) AS avgX, min(x) AS minX, max(x) AS maxX, avg(y) AS avgY, min(y) AS minY, max(y) AS maxY, avg(z) AS avgZ, min(z) AS minZ, max(z) AS maxZ, min(ts) AS startTime, max(ts) AS endTime FROM (SELECT x, y, z, ts, NTILE($2) OVER (ORDER BY ts) AS w FROM " + sensor + " WHERE trip_id = $1{{LIMIT_TIME}}) A GROUP BY w ORDER BY w;";
 
-		if (query.window === undefined) { query.window = 200 }; // 966 =~ 1024kb, 483 =~ 1024kb/3
-
+		if (query.window === undefined) { query.window = 200; } // 966 =~ 1024kb, 483 =~ 1024kb/3
 		var values = [params.trip_id, query.window];
 
 		var limitTime = "";
@@ -144,7 +127,7 @@ module.exports = {
 		var statement = {
 			text: text.replace("{{LIMIT_TIME}}", " " + limitTime),
 			values: values
-		}
+		};
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
@@ -152,49 +135,36 @@ module.exports = {
 		});
 	},
 
-	// raw: function(params, query, callback) {
-	// 	var acc = this.window(params, query, callback, function(err, result) {
-	// 		if (err) { console.error(err); return; }
-	// 		console.log(result);
-	// 		callback(null, result);
-	// 	});
-
-	// 	this.query(statement, function(err, result) {
-	// 		if (err) { console.error(err); return; }
-	// 		callback(null, result);
-	// 	});
-	// },
-
-	// FIXME: abstract? utilize index route?
+	// CHANGEME: abstract? utilize index route?
 	devices: function(callback) {
 		var statement = {
 			text: "SELECT DISTINCT (user_id) user_id from trip ORDER BY user_id DESC"
-		}
+		};
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
 			callback(null, result);
-	});
+		});
 	},
 
 	count: function(params, callback) {
+		var statement;
 		if (params.sensor !== undefined) {
 			switch(params.sensor) {
 				case "acc": sensor = "sensor_accelerometer"; break;
 				case "lac": sensor = "sensor_linear_acceleration"; break;
 				case "gra": sensor = "sensor_gravity"; break;
 			}
-			var statement = {
+			statement = {
 				text: "SELECT COUNT(ts) FROM " + sensor + " WHERE trip_id = $1",
 				values: [params.trip_id]
-			}
+			};
 		} else {
-			var statement = {
+			statement = {
 				text: "SELECT COUNT(*) FROM trip WHERE user_id = $1",
 				values: [params.uuid]
-			}
+			};
 		}
-
 
 		this.query(statement, function(err, result) {
 			if (err) { console.error(err); return; }
@@ -203,4 +173,4 @@ module.exports = {
 	}
 
 // BOT
-}
+};
