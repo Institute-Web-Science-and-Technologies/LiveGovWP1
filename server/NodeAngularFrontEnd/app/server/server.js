976293b3b34f9298a6b1f7d8266359f5a219a509
René Wilhelm <rene.wilhelm@gmail.com>
2014-03-10 14:06:41 +0100

    Corrected static paths due to directory structure cleanups

--- server/NodeAngularFrontEnd/app/server/server.js
+++ server/NodeAngularFrontEnd/app/server/server.js
@@ -2,14 +2,15 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', ('../client/views'));
+app.set('views', (__dirname + '/../client/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(require('stylus').middleware('../client/public'));
-app.use(express.static('../client/public'));
+app.use('/lib', express.static(__dirname + '/../client/bower_components'));
+app.use('/app', express.static(__dirname + '/../client/public'));
 app.use(app.router); // put below static!
 
 if ('development' == app.get('env')) {
