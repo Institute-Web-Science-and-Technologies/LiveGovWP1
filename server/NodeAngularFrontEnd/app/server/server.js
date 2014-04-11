d9b5652089d5c3fc2320bab3bbb883c27e9d2578
René Wilhelm <rene.wilhelm@gmail.com>
2014-04-11 19:15:18 +0200

    Added compression. Removed stylus in favour of sass.

--- server/NodeAngularFrontEnd/app/server/server.js
+++ server/NodeAngularFrontEnd/app/server/server.js
@@ -1,16 +1,20 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', (__dirname + '/../client/views'));
+app.set('views', path.join(__dirname, '/../client/views'));
 app.set('view engine', 'jade');
+
 app.use(express.favicon());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(require('stylus').middleware('../client/public'));
-app.use('/lib', express.static(__dirname + '/../client/bower_components'));
-app.use('/app', express.static(__dirname + '/../client/public'));
+app.use(express.compress());
+
+app.use('/app', express.static(path.join(__dirname, '/../client/public')));
+app.use('/lib', express.static(path.join(__dirname, '/../client/bower_components')));
+
 app.use(app.router); // put below static!
 
 if ('development' == app.get('env')) {
