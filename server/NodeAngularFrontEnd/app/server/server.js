245c98d296936100181e3a8ecf0ea31bd60a9aba
René Wilhelm <rene.wilhelm@gmail.com>
2014-04-11 20:21:05 +0200

    Updated and migrated from Express 3.x to 4.x

--- server/NodeAngularFrontEnd/app/server/server.js
+++ server/NodeAngularFrontEnd/app/server/server.js
@@ -6,20 +6,17 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '/../client/views'));
 app.set('view engine', 'jade');
 
-app.use(express.favicon());
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.compress());
-
 app.use('/app', express.static(path.join(__dirname, '/../client/public')));
 app.use('/lib', express.static(path.join(__dirname, '/../client/bower_components')));
 
-app.use(app.router); // put below static!
+// middleware
+app.use(require('body-parser')()); // previously bodyParser, json and urlencoded
+app.use(require('method-override')());
+app.use(require('compression')());
 
 if ('development' == app.get('env')) {
-	app.use(express.logger('dev'));
-	app.use(express.errorHandler());
+  app.use(require('morgan')()); // logger
+  app.use(require('errorhandler')());
 }
 
 require('./routes')(app);
