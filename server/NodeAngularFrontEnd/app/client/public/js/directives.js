130b1a2e6ea62a28cbc05f4e1c87041b9a9f6b25
René Wilhelm <rene.wilhelm@gmail.com>
2014-04-13 03:02:54 +0200

    Fixing charts.
    Still very buggy.

--- server/NodeAngularFrontEnd/app/client/public/js/directives.js
+++ server/NodeAngularFrontEnd/app/client/public/js/directives.js
@@ -1,24 +1,33 @@
 (function() {
   'use strict';
 
+  var brush = d3.custom.chartBrush();
+  var brushElement;
+
   app.directive('brush', [function () {
-    var brush = d3.custom.chartBrush();
     return {
+      // priority: 2,
       restrict: 'E',
       scope: {
         domain: '=', extent: '=', onBrushExtent: '&', loadMoreData: '&'
       },
+      // controller: function () {
+      //   this.setExtent = function (extent) {
+      //     console.log("brush ctrl says:", extent);
+      //     brushElement.call(brush.extent(extent));
+      //   };
+      // },
       link: function ($scope, $element, $attributes) {
-        var brushElement = d3.select($element[0]);
+        brushElement = d3.select($element[0]);
 
         brush.on('brushed', function (d, i) {
           // EXTENT 2:
-          console.log('EXTENT 2:', d, $scope.extent);
+          console.log('BRUSH EXTENT 2:', d, $scope.extent);
           $scope.onBrushExtent({args:d});
         });
 
         brush.on('brushended', function (d, i) {
-          $scope.loadMoreData({args: d});
+          // $scope.loadMoreData({args: d});
         });
 
         brush.on('ready', function (d, i) {
@@ -38,63 +47,79 @@
         // how to check if brush is already drawn?
 
         // extent = Array[2]
-        $scope.$watchCollection('extent', function (extent, oldExtent) {
-          if (extent && extent.length) {
-            console.log('EXTENT 4:', extent, oldExtent);
-            brushElement.call(brush.extent(extent));
-          }
-        });
-      }
-    };
-  }]);
-
-  app.directive('chart', [function () {
-    var chart = d3.custom.lineChart();
-    return {
-      restrict: 'E',
-      scope: {
-        data: '=', domain: '=', extent: '=', onBrushExtent: '&', loadMoreData: '&'
-      },
-      link: function ($scope, $element, $attributes) {
-        var chartElement = d3.select($element[0]);
-
-        chart.on('brushended', function(d, i) {
-          $scope.loadMoreData({args:d});
-          $scope.onBrushExtent({args:d}); // update scope
-        });
-
-        // chart.on('ready', function (d, i) {
-        //   // console.info('unwatching y domain!');
-        //   // unwatchDomain();
-        // });
-
-        // var unwatchDomain =
-        // $scope.$watchCollection('domain', function (val, oldVal) {
-        //   if (val) {
-        //     chartElement.call(chart.yScale(val.y));
-        //     chartElement.call(chart.xScale(val.x));
-        //     // console.info('updating y domain!');
+        // $scope.$watchCollection('extent', function (extent, oldExtent) {
+        //   if (extent && extent.length) {
+        //     console.log('BRUSH EXTENT 4:', extent, oldExtent);
+        //     brushElement.call(brush.extent(extent));
         //   }
         // });
+}
+};
+}]);
+
+app.directive('chart', [function () {
+  var chart = d3.custom.lineChart();
+  return {
+    // priority: 1,
+    restrict: 'E',
+    // require: '^brush',
+    scope: {
+      data: '=', domain: '=', extent: '=', onBrushExtent: '&', loadMoreData: '&'
+    },
+    link: function ($scope, $element, $attributes, brushCtrl) {
+      var chartElement = d3.select($element[0]);
+
+      chart.on('brushended', function(d, i) {
+          // EXTENT 2:
+          console.log('CHART EXTENT 2:', d, $scope.extent);
+          $scope.onBrushExtent({args:d});
+          // $scope.loadMoreData({args:d});
+          // CALL BRUSH SCOPE FROM HERE AND CHANGE EXTENT!
+          // brushCtrl.setExtent(d);
+          // brushElement.call(brush.extent(extent))
+          brushElement.call(brush.extent(d));
+        });
+
+      chart.on('ready', function (d, i) {
+          // console.info('unwatching y domain!');
+          // unwatchDomain();
+        });
+
+      var unwatchDomain =
+      $scope.$watchCollection('domain', function (domain, oldDomain) {
+        if (unwatchData && domain && domain.x.length && domain.y.length) {
+          chartElement.call(chart.yScale(domain.y));
+          chartElement.call(chart.xScale(domain.x));
+            // console.info('updating y domain!');
+          }
+        });
 
         // STEP 1: as soon as chart data arrives, draw the chart
+        var unwatchData =
         $scope.$watchCollection('data', function (data, oldData) {
           // console.log($scope.data.length);
           // if (data && data.length && $scope.domain.x.length && $scope.domain.y.length) {
-          if (data && data.length) {
-            chartElement.datum({data:data}).call(chart);
+            if (data && data.length) {
+              chartElement.datum({data:data}).call(chart);
             // chartElement.datum({data:$scope.data, domain:$scope.domain}).call(chart);
           }
         });
 
         // if the brush extent changes, change the charts x-domain accordingly
+        $scope.$watchCollection('extent', function (extent, oldExtent) {
+          if (extent && extent.length) {
+            console.log('CHART EXTENT 4:', extent, oldExtent);
+            chartElement.call(chart.extent(extent));
+          }
+        });
+
         // $scope.$watchCollection('extent', function (val, oldVal) {
         //   if (val !== oldVal) {
         //     // console.log('extent updated!', val);
         //     chartElement.call(chart.x($scope.extent));
         //   }
         // });
-      }
-    };
-  }]);
+}
+};
+}]);
 }());
--- server/NodeAngularFrontEnd/app/client/public/js/reusable/chart.js
+++ server/NodeAngularFrontEnd/app/client/public/js/reusable/chart.js
@@ -9,7 +9,7 @@
 
     // default values. may be overwritten by exported functions.
 
-    var margin = {top: 8, right: 8, bottom: 8, left: 8},
+    var margin = {top: 30, right: 30, bottom: 30, left: 30},
         width = d3.select(this)[0].parentNode.offsetWidth - ((margin.left + margin.right)*3) - 1,
         height = 120,
         xScale = d3.time.scale().range([0, width]),
@@ -19,24 +19,21 @@
         brush = d3.svg.brush(),
         extent = [];
 
-    // console.log(width);
-
     var svg;
 
     var dispatch = d3.dispatch('brushended', 'ready');
 
-    function exports(_selection) {
-      _selection.each(function (d, i) {
+    function exports(selection) {
+      selection.each(function (d, i) {
+        if (!d) return;
 
         // REQUIREMENTS
-        // data = Array[n]
-
-        // if (!d) return;
+        // d.data = Array[n]
 
         var data = d.data;
 
-        // var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(Math.max(width/75, 2)),
-        //     yAxis = d3.svg.axis().scale(y).orient("left").ticks(Math.max(height/25, 2));
+        // var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(Math.max(width/75, 2)),
+        //     yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(Math.max(height/25, 2));
 
         var line = d3.svg.line().interpolate("linear")
           .x(function(d) { return xScale(d[0]); })
@@ -53,11 +50,11 @@
                   .attr("id", "clip")
                 .append("rect")
                   .attr("width", width)
-                  .attr("height", height)
-                  .attr('width', '100%');
+                  .attr("height", height);
+                  // .attr('width', '100%');
+        } else {
+          d3.select(this).selectAll('svg').selectAll("*").remove();
         }
-
-        d3.select(this).selectAll('svg').selectAll("*").remove();
 
         svg = d3.select(this).selectAll('svg').data(data);
 
@@ -95,8 +92,11 @@
           .attr("transform", "translate(-3,0)")
           .call(yAxis);
 
+        if (extent.length) xScale.domain(extent);
+        
         brush.x(xScale)
           .on("brushend", brushended);
+
 
         var gBrush = chart.append("g")
           .attr("class", "brush")
@@ -104,30 +104,33 @@
         .selectAll("rect")
           .attr("height", height);
 
-        if (extent && extent.length) x.domain(extent);
 
         function brushended() {
-          if (brush.extent()[0] == brush.extent()[1]) return;
-          // if (brush.empty()) return;
+          if (brush.empty()) return;
 
-          dispatch.brushended(brush.extent().map(function (d) { return +d }));
-          // dispatch.brushended(brush.extent().map(function(d) { return +d; })); // update brush extent in scope
-
+          // scale x domain to brush extent (zoom in)
           xScale.domain(brush.extent());
 
+          console.log('CHART EXTENT 1:', brush.extent().map(function (d) { return +d; }), extent);
+          // dispatch.brushed(brush.empty() ? [] : brush.extent().map(function (d) { return +d; }));
+
+          // redraw the graph lines
           svg.selectAll(".line0").attr("d", line(data.map(function (d) { return [d.ts, d.avgx]; })));
           svg.selectAll(".line1").attr("d", line(data.map(function (d) { return [d.ts, d.avgy]; })));
           svg.selectAll(".line2").attr("d", line(data.map(function (d) { return [d.ts, d.avgz]; })));
 
-          svg.select(".x.axis").call(xAxis);
+          // rescale x axis
+          // svg.select(".x.axis").call(xAxis);
 
-          // after brushing clear the brush and call a new one
+          brush.clear();
+          reset_axis();
+          // clear the brush and call a new one
           chart.select("brush").call(brush.clear());
-          chart.select("brush").call(brush);
+          // chart.select("brush").call(brush);
         }
 
         function reset_axis() {
-          svg //.transition().duration(500)
+          svg.transition().duration(500)
             .select(".x.axis")
             .call(xAxis);
         }
@@ -168,6 +171,18 @@
       if (!arguments.length) return yScale;
       yScale = yScale.range([height, 0]).domain(_);
       // dispatch.ready(); // stop watching for y domain
+      return this;
+    };
+
+    exports.extent = function(_) {
+      if (!arguments.length) return extent;
+
+      // _ is the incoming extent value
+      // extent is the old extent value
+
+      console.log('CHART EXTENT 5:', _, extent);
+      // console.log('\n--- END OF BRUSH CYCLE -----------------------------------------------\n');
+      extent = _;
       return this;
     };
 
