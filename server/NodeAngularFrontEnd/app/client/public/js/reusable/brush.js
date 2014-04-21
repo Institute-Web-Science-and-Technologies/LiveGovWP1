a39bfa07f2a7b000e45033be353cb626e0339afa
René Wilhelm <rene.wilhelm@gmail.com>
2014-04-21 16:32:31 +0200

    Simplifications and clean ups

--- server/NodeAngularFrontEnd/app/client/public/js/reusable/brush.js
+++ server/NodeAngularFrontEnd/app/client/public/js/reusable/brush.js
@@ -1,112 +1,82 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var t = new Date();
+if (!d3.custom) d3.custom = {};
 
-  if (!d3.custom) d3.custom = {};
+d3.custom.chartBrush = function module() {
 
-  d3.custom.chartBrush = function module() {
+  // default values. may be overwritten by exported functions.
 
-    // default values. may be overwritten by exported functions.
+  var margin = {top: 8, right: 8, bottom: 8, left: 8 }, // FIXME
+    width = d3.select(this)[0].parentNode.offsetWidth - ((margin.left + margin.right) * 3) - 1,
+    height = 64,
+    xScale = d3.time.scale().range([0, width]),
+    yScale = d3.scale.linear().range([height, 0]),
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(Math.max(width / 75, 2)),
+    yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(Math.max(height / 25, 2)),
+    brush = d3.svg.brush(),
+    extent = [];
 
-    var margin = {top: 8, right: 8, bottom: 8, left: 8},
-        width = d3.select(this)[0].parentNode.offsetWidth - ((margin.left + margin.right) * 3) - 1,
-        height = 64,
-        xScale = d3.time.scale().range([0, width]),
-        yScale = d3.scale.linear().range([height, 0]),
-        xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(Math.max(width/75, 2)),
-        yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(Math.max(height/25, 2)),
-        brush = d3.svg.brush(),
-        extent = [];
+  var dispatch = d3.dispatch('brushed', 'brushended'); // exposed functions
 
-    var svg;
+  function exports(selection) {
+    selection.each(function(d, i) {
+      if (!d) return;
 
-    var dispatch = d3.dispatch('brushed', 'brushended', 'ready');
+      var domain = d.domain; // d.domain = { 'x': Array[2], 'y': Array[2] }
 
-    function exports(selection) {
-      selection.each(function (d, i) {
-        if (!d) return;
+      xScale.domain(domain.x);
+      yScale.domain(domain.y);
 
-        // REQUIREMENTS
-        // d.domain = { 'x': Array[2], 'y': Array[2] }
+      // create svg element if it doesn't exist
+      if (d3.select(this).select('svg').empty()) {
+        d3.select(this)
+          .append('svg')
+            .classed('brush', true)
+            .attr("width", width)
+            .attr("height", height);
+      }
 
-        var domain = d.domain;
+      // select svg element
+      var svg = d3.select(this).select('svg');
 
-        // FIXME: domain must be updated after final domain is calculated!
-        dispatch.ready(); // stop watching for domain
+      // remove old children of svg element
+      svg.selectAll("*").remove();
 
-        xScale.domain(domain.x);
-        yScale.domain(domain.y);
+      // create brush
+      brush.x(xScale)
+        .on("brush", brushed)
+        .on("brushend", brushended);
 
-        if (!d3.select(this).select('svg')[0][0]) {
-          svg = d3.select(this)
-            .append('svg')
-              .classed('brush', true)
-              .attr("width", width)
-              .attr("height", height);
-        }
+      // programatically set brush extent if it's set
+      if (extent.length) brush.extent(extent);
 
-        // so important...
-        svg.selectAll("*").remove();
-
-        brush.x(xScale)
-          .on("brush", brushed)
-          .on("brushend", brushended);
-
-        // programatically set brush extent, if extent differs from default value []
-        if (extent.length) brush.extent(extent);
-
-        // draw the actual brush rectangle
-        var gBrush = svg.append("g")
-          .attr("class", "brush")
-          .call(brush)
+      // draw the actual brush rectangle
+      svg.append("g")
+        .attr("class", "brush")
+        .call(brush)
         .selectAll("rect")
-          .attr("height", height - 1); // ?
+        .attr("height", height - 1); // ?
 
-        function brushed() {
-          // EXTENT 1:
-          // console.log('BRUSH EXTENT 1:', brush.extent().map(function (d) { return +d; }), extent);
-          dispatch.brushed(brush.empty() ? [] : brush.extent().map(function (d) { return +d; }));
-        }
+      function brushed() {
+        // console.log('BRUSH EXTENT 1:', brush.extent().map(function (d) { return +d; }), extent);
+        dispatch.brushed(brush.empty() ? [] : brush.extent().map(function(d) { return +d; }));
+      }
 
-        function brushended() {
-          if (!d3.event.sourceEvent) return; // only transition after input
-          dispatch.brushended(brush.extent().map(function(d) { return +d; }));
-        }
-      });
-    }
+      function brushended() {
+        if (!d3.event.sourceEvent) return; // only transition after input
+        dispatch.brushended(brush.extent().map(function(d) { return +d; }));
+      }
+    });
+  }
 
-    exports.width = function(_) {
-      if (!arguments.length) return width;
-      width = parseInt(_);
-      return this;
-    };
-
-    exports.height = function(_) {
-      if (!arguments.length) return height;
-      height = parseInt(_);
-      duration = 0;
-      return this;
-    };
-
-    exports.extent = function(_) {
-      if (!arguments.length) return extent;
-
-      // _ is the incoming extent value
-      // extent is the old extent value
-
-      // console.log('BRUSH EXTENT 5:', _, extent);
-      // console.log('\n--- END OF BRUSH CYCLE -----------------------------------------------\n');
-      extent = _;
-      return this;
-    };
-
-    d3.rebind(exports, dispatch, 'on');
-    return exports;
+  // expose extent setter/getter
+  exports.extent = function(_) {
+    if (!arguments.length) return extent;
+    // console.log('BRUSH EXTENT 5:', _, extent);
+    extent = _;
+    return this; // calls chartBrush
   };
 
-  // document.addEventListener("DOMContentLoaded", function (event) {
-  //   console.log("DOM fully loaded and parsed after " + (event.timeStamp - t) + " ms");
-  // });
-
-}());
+  d3.rebind(exports, dispatch, 'on');
+  return exports;
+};
--- server/NodeAngularFrontEnd/app/client/public/js/reusable/chart.js
+++ server/NodeAngularFrontEnd/app/client/public/js/reusable/chart.js
@@ -1,232 +1,195 @@
-(function () {
-  'use strict';
+/* jshint strict:true, devel:true, debug:true */
+/* globals d3 */
+'use strict'; // jshint -W097
 
-  // var t = new Date();
+if (!d3.custom) d3.custom = {};
 
-  if (!d3.custom) d3.custom = {};
+d3.custom.lineChart = function module() {
 
-  d3.custom.lineChart = function module() {
+  // default values. may be overwritten by exported functions.
 
-    // default values. may be overwritten by exported functions.
+  var margin = {top: 8, right: 8, bottom: 40, left: 40 }, // FIXME
+    width = d3.select(this)[0].parentNode.offsetWidth - ((margin.left + margin.right) * 3) - 1,
+    height = 120,
+    xScale = d3.time.scale().range([0, width]),
+    yScale = d3.scale.linear().range([height, 0]),
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(Math.max(width / 75, 2)),
+    yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(Math.max(height / 25, 2)),
+    brush = d3.svg.brush(),
+    extent = [];
 
-    var margin = {top: 8, right: 8, bottom: 40, left: 40},
-        width = d3.select(this)[0].parentNode.offsetWidth - ((margin.left + margin.right)*3) - 1,
-        height = 120,
-        xScale = d3.time.scale().range([0, width]),
-        yScale = d3.scale.linear().range([height, 0]),
-        xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(Math.max(width/75, 2)),
-        yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(Math.max(height/25, 2)),
-        brush = d3.svg.brush(),
-        extent = [];
+  var dispatch = d3.dispatch('brushended'); // exposed functions
 
-    var svg;
+  function exports(selection) {
+    selection.each(function(d, i) {
 
-    var dispatch = d3.dispatch('brushended', 'ready');
+      debugger
 
-    function exports(selection) {
-      selection.each(function (d, i) {
-        if (!d) return;
+      if (!d) return;
 
-        var data = d.data;
+      var data = d.data;
 
-        var chartName = this.getAttribute('data').split('.')[1];
+      var chartName = this.classList[0];
 
-        // FIXME
-        if (extent.length) {
-          xScale.domain(extent);
-        } else {
-          xScale.domain(d3.extent([].concat.apply([], data.map(function(d) {return [d.starttime, d.endtime]; }))));
-        }
-          
-        var line = d3.svg.line().interpolate("linear")
-          .x(function(d) { return xScale(d[0]); })
-          .y(function(d) { return yScale(d[1]); });
+      // FIXME don't recalculate extent
+      // if (extent.length) {
+      //   xScale.domain(extent);
+      // } else {
+      //   dispatch('domain')
+      //   xScale.domain(data.extent([d.starttime, d.endtime]));
+      //   }))));
+      // }
 
-        console.log('this', chartName, this);
-        console.log('svg', chartName, svg);
+      // FIXME expose
+      var line = d3.svg.line().interpolate("linear")
+        .x(function(d) {
+          return xScale(d[0]);
+        })
+        .y(function(d) {
+          return yScale(d[1]);
+        });
 
-        // FIXME svg is probably selected wrong and causes all the trouble
-        if (d3.select(this).select('svg').empty()) { // why not (!svg)?
-          svg = d3.select(this)
-            .append('svg')
-              .classed('chart', true)
-              .classed(chartName, true)
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-            .append("defs")
-            .append("clipPath")
-              .attr("id", "clip")
-            .append("rect")
-              .attr("width", width)
-              .attr("height", height);
-        }
+      if (d3.select(this).select('svg').empty()) {
+        d3.select(this)
+          .append('svg')
+            .classed('chart', true)
+            .classed(chartName, true)
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("defs")
+          .append("clipPath")
+            .attr("id", "clip")
+          .append("rect")
+            .attr("width", width)
+            .attr("height", height);
+      }
 
-        d3.select(this).select('svg').selectAll("*").remove();
-        
-        svg = d3.select(this).select('svg').data(data);
+      // remove old children of svg element
+      d3.select(this).select('svg').selectAll("*").remove();
 
-        brush.x(xScale)
-          .on("brushend", brushended);
+      // select svg element
+      var svg = d3.select(this).select('svg').data(data);
 
-        var chart = svg.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-          .attr("class", "chart");
+      // create brush
+      brush.x(xScale)
+        .on("brushend", brushended);
 
-        // x-graph
-        chart.append("path")
-          .attr("class", "line line0")
-          .attr("clip-path", "url(#clip)")
-          .attr("d", line(data.map(function(d) { return [d.ts, d.avgx]; })));
+      var chart = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("class", "chart");
 
-        // y-graph
-        chart.append("path")
-          .attr("class", "line line1")
-          .attr("clip-path", "url(#clip)")
-          .attr("d", line(data.map(function(d) { return [d.ts, d.avgy]; })));
+      // x-graph
+      chart.append("path")
+        .attr("class", "line line0")
+        .attr("clip-path", "url(#clip)")
+        .attr("d", line(data.map(function(d) { return [d.ts, d.avgx]; })));
 
-        // z-graph
-        chart.append("path")
-          .attr("class", "line line2")
-          .attr("clip-path", "url(#clip)")
-          .attr("d", line(data.map(function(d) { return [d.ts, d.avgz]; })));
+      // y-graph
+      chart.append("path")
+        .attr("class", "line line1")
+        .attr("clip-path", "url(#clip)")
+        .attr("d", line(data.map(function(d) { return [d.ts, d.avgy]; })));
 
-        // x-axis
-        chart.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + (height + 3) + ")")
-          .call(xAxis);
+      // z-graph
+      chart.append("path")
+        .attr("class", "line line2")
+        .attr("clip-path", "url(#clip)")
+        .attr("d", line(data.map(function(d) { return [d.ts, d.avgz]; })));
 
-        // y-axis
-        chart.append("g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(-3,0)")
-          .call(yAxis);
+      // x-axis
+      chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (height + 3) + ")") // ?
+        .call(xAxis);
 
-        chart.append("g")
-          .attr("class", "brush")
-          .call(brush)
+      // y-axis
+      chart.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(-3,0)") // ?
+        .call(yAxis);
+
+      var gBrush = chart.append("g")
+        .attr("class", "brush")
+        .call(brush)
         .selectAll("rect")
-          .attr("height", height);
+        .attr("height", height);
 
+      // if (extent.length) gBrush.call(brush.extent(extent));
 
-        // console.log(chartName, 'CHART DOMAIN XSCALE', xScale.domain().map(function (d) { return +d; }));
-        // console.log(chartName, 'CHART DOMAIN YSCALE', yScale.domain());
-        // console.log(chartName, 'CHART RANGE  XSCALE', xScale.range());
-        // console.log(chartName, 'CHART RANGE  YSCALE', yScale.range());
-        // console.log('--------------------------------------------------');
+      function brushended() {
+        // console.log('CHART EXTENT 1:', brush.extent().map(function (d) { return +d; }), extent);
+        dispatch.brushended(brush.empty() ? [] : brush.extent().map(function(d) { return +d; }));
 
+        // this first clears the brush, and then tells it to redraw
+        // https://groups.google.com/d/msg/d3-js/SN4-kJD6_2Q/SmQNwLm-5bwJ
+        chart.select(".brush").call(brush.clear());
 
-        function brushended() {
-          // if (brush.empty()) return;
+        xScale.domain(brush.extent()); // scale the x-domain to the extent (zoom in)
 
-          // console.log('CHART EXTENT 1:', brush.extent().map(function (d) { return +d; }), extent);
-          dispatch.brushended(brush.empty() ? [] : brush.extent().map(function (d) { return +d; }));
+        svg.transition().duration(2000).select(".x.axis").call(xAxis);
 
-          // this first clears the brush, and then tells it to redraw
-          // https://groups.google.com/d/msg/d3-js/SN4-kJD6_2Q/SmQNwLm-5bwJ
-          chart.select(".brush").call(brush.clear());
+        svg
+          .transition()
+          .duration(2000)
+          .selectAll(".line0")
+          .attr("d", line(data.map(function(d) {return [d.ts, d.avgx]; })));
 
-          console.log(chartName, brush.extent());
-          xScale.domain(brush.extent()); // scale the x-domain to the extent (zoom in)
+        svg
+          .transition()
+          .duration(2000)
+          .selectAll(".line1")
+          .attr("d", line(data.map(function(d) {return [d.ts, d.avgy]; })));
 
-          // transition is for debugging purposes (to see what's happening)
+        svg
+          .transition()
+          .duration(2000)
+          .selectAll(".line2")
+          .attr("d", line(data.map(function(d) {return [d.ts, d.avgz]; })));
+      }
 
-          // FIXME only the axis of the last drawn chart is selected, not 'this' one
-          svg.transition().duration(2000).select(".x.axis").call(xAxis);
+      // function transition_data() {
+      //   this.svg.selectAll(".line0").attr("d", line(data.map(function (d) { return [d.ts, d.avgx]; })));
+      //   this.svg.selectAll(".line1").attr("d", line(data.map(function (d) { return [d.ts, d.avgy]; })));
+      //   this.svg.selectAll(".line2").attr("d", line(data.map(function (d) { return [d.ts, d.avgz]; })));
+      // }
 
-          svg.transition().duration(2000).selectAll(".line0").attr("d", line(data.map(function (d) { return [d.ts, d.avgx]; })));
-          svg.transition().duration(2000).selectAll(".line1").attr("d", line(data.map(function (d) { return [d.ts, d.avgy]; })));
-          svg.transition().duration(2000).selectAll(".line2").attr("d", line(data.map(function (d) { return [d.ts, d.avgz]; })));
+      // function reset_axis() {
+      //   svg.transition().duration(500)
+      //     .select(".x.axis")
+      //     .call(xAxis);
+      // }
 
-          // chart.select("brush").call(brush.clear()); // clear brush
-          // chart.select("brush").call(brush); // call new brush
-          // svg.select(".x.axis").call(xAxis); // rescale x axis
+      // function clear_brush() {
+      //   if (!data) { console.error('clear_brush has no data'); return; }
+      //   xScale.domain(d3.extent([].concat.apply([], data.map(function (d) { return [d.starttime, d.endtime]; }))));
+      //   transition_data();
+      //   reset_axis();
+      // }
+    });
+  }
 
-          // scale x domain to brush extent (zoom in)
-          // brush.clear();
-          
-          // transition_data();
-          // reset_axis();
-          // brush.clear();
-          // chart.select("brush").call(brush.clear());
-          // chart.select("brush").call(brush);
-
-          // redraw the graph lines
-
-          // brush.clear();
-          // reset_axis();
-          // clear the brush and call a new one
-          // chart.select("brush").call(brush.clear());
-          // chart.select("brush").call(brush);
-          // clear_brush();
-        }
-
-        // function transition_data() {
-        //   this.svg.selectAll(".line0").attr("d", line(data.map(function (d) { return [d.ts, d.avgx]; })));
-        //   this.svg.selectAll(".line1").attr("d", line(data.map(function (d) { return [d.ts, d.avgy]; })));
-        //   this.svg.selectAll(".line2").attr("d", line(data.map(function (d) { return [d.ts, d.avgz]; })));
-        // }
-
-        // function reset_axis() {
-        //   svg.transition().duration(500)
-        //     .select(".x.axis")
-        //     .call(xAxis);
-        // }
-
-        // function clear_brush() {
-        //   if (!data) { console.error('clear_brush has no data'); return; }
-        //   xScale.domain(d3.extent([].concat.apply([], data.map(function (d) { return [d.starttime, d.endtime]; }))));
-        //   transition_data();
-        //   reset_axis();
-        // }
-      });
-    }
-
-    // clear brush means:
-    // 1. scale x domain to extent
-    // 2. draw the three lines
-    // 3. call x axis
-
-    exports.width = function(_) {
-      if (!arguments.length) return width;
-      width = parseInt(_);
-      return this;
-    };
-
-    exports.height = function(_) {
-      if (!arguments.length) return height;
-      height = parseInt(_);
-      duration = 0;
-      return this;
-    };
-
-    // to update the x-domain with the brush'es extent
-    exports.xScale = function(_) {
-      if (!arguments.length) return xScale;
-      xScale = xScale.domain(_);
-      return this;
-    };
-
-    // to update the y-domain when it changes
-    exports.yScale = function(_) {
-      if (!arguments.length) return yScale;
-      yScale = yScale.domain(_);
-      return this;
-    };
-
-    exports.extent = function(_) {
-      if (!arguments.length) return extent; // currently set value
-      // console.log('CHART EXTENT 5:', _, extent);
-      extent = _;
-      return this;
-    };
-
-    d3.rebind(exports, dispatch, 'on');
-    return exports;
+  // to update the x-domain with the brush'es extent
+  exports.xScale = function(_) {
+    if (!arguments.length) return xScale;
+    xScale = xScale.domain(_);
+    return this;
   };
 
-  // document.addEventListener("DOMContentLoaded", function (event) {
-  //   console.log("DOM fully loaded and parsed after " + (event.timeStamp - t) + " ms");
-  // });
+  // to update the y-domain when it changes
+  exports.yScale = function(_) {
+    if (!arguments.length) return yScale;
+    yScale = yScale.domain(_);
+    return this;
+  };
 
-}());
+
+  exports.extent = function(_) {
+    if (!arguments.length) return extent;
+    // console.log('CHART EXTENT 5:', _, extent);
+    extent = _;
+    return this;
+  };
+
+  d3.rebind(exports, dispatch, 'on');
+  return exports;
+};
