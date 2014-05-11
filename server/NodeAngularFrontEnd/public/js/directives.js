8b08dcb919d62c14be31a51d7ae962dde41095fe
René Wilhelm <rene.wilhelm@gmail.com>
2014-05-11 19:29:08 +0200

    Map directive.
    Refactor!

--- server/NodeAngularFrontEnd/public/js/directives.js
+++ server/NodeAngularFrontEnd/public/js/directives.js
@@ -79,71 +79,109 @@ app.directive('chart', [
   }
 ]);
 
-app.directive('map', [function () {
-  return {
-    restrict: 'E',
-    scope: {
-      data: '='
-    },
-    link: function ($scope, $element, $attributes) {
+app.directive('map', [
+  function () {
+    return {
+      restrict: 'E',
+      scope: {trip: '='},
+      link: function ($scope, $element, $attributes) {
+
+        // 1. create map
+        // 2. pepare activities FIXME put in trip object (all model logic in services!)
+        // 2. draw geojson to map
+        // 3. add legend
+
+        var map = L.mapbox.map('map', 'rene.i6mdi15p'); // mapbox id
+
+        var legend = L.control({
+          position: 'topright'
+        });
 
 
+        var geoJson;
 
 
+        var activities = $scope.trip.data.geo.features.map(function (d) {
+          return d.properties.activity;
+        });
 
-      // Leaflet Chloropleth Legend Example
-      var legend = L.control({
-        position: 'topright'
-      });
+        // FIXME -> HELPERS
+        activities[activities.indexOf(null)] = "unknown"; // change null to "unknown"
 
-      legend.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend leaflet-bar', this.legend);
+        // FIXME -> HELPERS
+        activities = activities.sort().filter(function (el, i, a) {
+          if (i == a.indexOf(el)) return 1;
+          return 0;
+        }); // sort unique
 
-        // sort activities before generating legend
-        var a = sortActivities(activities).filter(function (n) { return n; });
+        // FIXME -> HELPERS
+        activities.filter(function (n) {
+          return n;
+        }); // remove undefined
 
-        for (var i = 0; i < a.length; i++) {
-          div.innerHTML += '<i style="background:' + getColor(a[i]) + '"></i>' + a[i] + '<br>';
+        function drawLegend() {
+
+          // FIXME -> HELPERS(?)
+          // give array as argument (abstract!)
+          // returns activities in a manually sorted order
+          function sortActivities(activities) {
+            var a = ['driving', 'running', 'walking', 'standing', 'sitting', 'on table', 'unknown'];
+            return a.map(function (d) {
+              return activities[activities.indexOf(d)];
+            });
+          }
+
+          // requires sortActivities
+          legend.onAdd = function (map) {
+            var div = L.DomUtil.create('div', 'info legend leaflet-bar', this.legend);
+
+            // sort activities before generating legend
+            var a = sortActivities(activities).filter(function (n) { return n; });
+
+            for (var i = 0; i < a.length; i++) {
+              div.innerHTML += '<i style="background:' + getColor(a[i]) + '"></i>' + a[i] + '<br>';
+            }
+            return div;
+          };
+
+        };
+
+        function onEachFeature(feature, layer) {
+
+          // create a popup for each feature
+          if (feature.properties) {
+            var popupString = '<div class="popup">';
+            for (var k in feature.properties) {
+              var v = feature.properties[k];
+              if (k == 't0' || k == 't1') v = moment.unix(parseInt(v)).utc().format("HH:mm:ss");
+              if (k == 'distance')        v = Math.round(v * 100, 12) / 100 + " m";
+              if (k == 'duration')        v = moment.duration(v).humanize();
+              if (k == 'speed')           v = v + ' km/h';
+              popupString += k + ': ' + v + '<br />';
+            }
+            popupString += '</div>';
+            layer.bindPopup(popupString, {
+              maxHeight: 200
+            });
+          }
+
+          // highlight feature on mouseover
+          layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+          });
+
         }
-        return div;
-      };
+
+
+
 
       function resetHighlight(e) {
         geoJson.resetStyle(e.target);
       }
 
-      function onEachFeature(feature, layer) {
-        // Create a popup for each feature
-        if (feature.properties) {
-          var popupString = '<div class="popup">';
-          for (var k in feature.properties) {
-            var v = feature.properties[k];
-            if (k == 't0' || k == 't1') v = moment.unix(parseInt(v)).utc().format("HH:mm:ss");
-            if (k == 'distance')        v = Math.round(v * 100, 12) / 100 + " m";
-            if (k == 'duration')        v = moment.duration(v).humanize();
-            if (k == 'speed')           v = v + ' km/h';
-            popupString += k + ': ' + v + '<br />';
-          }
-          popupString += '</div>';
-          layer.bindPopup(popupString, {
-            maxHeight: 200
-          });
-        }
-        // Highlight feature on mouseover
-        layer.on({
-          mouseover: highlightFeature,
-          mouseout: resetHighlight,
-        });
-      }
 
-      // returns activities in a manually sorted order
-      function sortActivities(activities) {
-        var a = ['driving', 'running', 'walking', 'standing', 'sitting', 'on table', 'unknown'];
-        return a.map(function (d) {
-          return activities[activities.indexOf(d)];
-        });
-      }
-
+      // required by style
       function getColor(d) {
         switch (d) {
           case 'driving'  : return '#377eb8';
@@ -157,11 +195,12 @@ app.directive('map', [function () {
         }
       }
 
+      // requires getColor
       function style(feature) {
         return {
           weight: 8,
           opacity: 0.7,
-          color: getColor(feature.properties.activity),
+          color: getColor(feature.properties.activity)
         };
       }
 
@@ -181,53 +220,39 @@ app.directive('map', [function () {
 
 
 
-      // SETUP MAP
+      $scope.$watchCollection('trip.data.geo', function(val, oldVal) {
+        if (Object.keys($scope.trip.data.geo).length) {
+          console.log('geo data is ready!', $scope.trip.data.geo);
 
-      var map = new L.Map('map', {
-        zoom: 15,
-        minZoom: 8,
-        maxZoom: 24,
-        scrollWheelZoom: false
+          // map.featureLayer.setGeoJSON(val);
+
+          // legend.addTo(map);
+
+          // Draw geoJSON object to map
+          geoJson  = L.geoJson($scope.trip.data.geo, {
+            style: style,
+            onEachFeature: onEachFeature
+          }).addTo(map);
+
+          // Zoom map to fit our route
+          map.fitBounds(geoJson.getBounds());
+
+          map.addControl(L.mapbox.legendControl());
+        } else {
+          console.log('no geo data', $scope.trip.data.geo.length);
+        }
       });
 
-      // CloudMade
-      L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png?token={token}', {
-        attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-        key: 'a04e905a7d8448d8b412b7371dfa21be', // rene
-        token: '88fd62dfa481446aa8ac3c22bcb64d21'
-      }).addTo(map);
 
-      var data = $scope.data; // data is feature collection
 
-      if (!data || data.length === 0) { console.log('no data :/'); return; }
+      // // Draw geoJSON object to map
+      // var geoJson = L.geoJson(data, {
+      //   style: style,
+      //   onEachFeature: onEachFeature
+      // }).addTo(map);
 
-      console.log(data);
-
-      var activities = data.features.map(function (d) {
-        return d.properties.activity;
-      });
-
-      activities[activities.indexOf(null)] = "unknown"; // change null to "unknown"
-
-      activities = activities.sort().filter(function (el, i, a) {
-        if (i == a.indexOf(el)) return 1;
-        return 0;
-      }); // sort unique
-
-      activities.filter(function (n) {
-        return n;
-      }); // remove undefined
-
-      legend.addTo(map);
-
-      // Draw geoJSON object to map
-      var geoJson = L.geoJson(data, {
-        style: style,
-        onEachFeature: onEachFeature
-      }).addTo(map);
-
-      // Zoom map to fit our route
-      map.fitBounds(geoJson.getBounds());
+      // // Zoom map to fit our route
+      // map.fitBounds(geoJson.getBounds());
 
     } // end link
   }; // end return
