8f8eee3c24cb9e423b4f1803d580061024d33c47
René Wilhelm <rene.wilhelm@gmail.com>
2014-06-07 16:04:42 +0200

    Array unique prototype (untested)

--- server/NodeAngularFrontEnd/public/js/helpers.js
+++ server/NodeAngularFrontEnd/public/js/helpers.js
@@ -40,6 +40,14 @@
       }
     }).filter(function(d) { return d; });
   };
+
+  Array.prototype.unique = function() {
+      var o = {}, i, l = this.length, r = [];
+      for(i=0; i<l;i+=1) o[this[i]] = this[i];
+      for(i in o) r.push(o[i]);
+      return r;
+  };
+
   // Object.prototype.getById = function(id) {
   //   return this.filter()
 
