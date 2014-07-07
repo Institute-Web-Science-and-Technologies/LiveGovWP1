ece5c028dfd6a29a11f967b58b924753fdb8a51e
Lukas Härtel <lukashaertel@googlemail.com>
2014-07-07 18:36:17 +0200

    Fixed android location provider

--- mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/sensors/sensor_producers/LocationHolderAndroid.java
+++ mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/sensors/sensor_producers/LocationHolderAndroid.java
@@ -1,6 +1,7 @@
 package eu.liveandgov.wp1.sensor_collector.sensors.sensor_producers;
 
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,6 +10,8 @@ import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.google.android.gms.location.LocationRequest;
 
 import eu.liveandgov.wp1.sensor_collector.GlobalContext;
 import eu.liveandgov.wp1.sensor_collector.configuration.SensorCollectionOptions;
@@ -19,14 +22,12 @@ import static junit.framework.Assert.assertNotNull;
 /**
  * Created by lukashaertel on 04.12.13.
  */
-public class LocationHolderAndroid extends LocationHolder
-{
+public class LocationHolderAndroid extends LocationHolder {
     private static final String LOG_TAG = "LOC_A";
 
     private final Looper looper;
 
-    public LocationHolderAndroid(SensorQueue sensorQueue, Looper looper)
-    {
+    public LocationHolderAndroid(SensorQueue sensorQueue, Looper looper) {
         super(sensorQueue);
         this.looper = looper;
     }
@@ -35,7 +36,15 @@ public class LocationHolderAndroid extends LocationHolder
     public void startRecording() {
         checkEnableAndroidGPS();
 
-        GlobalContext.getLocationManager().requestLocationUpdates(LocationManager.GPS_PROVIDER, SensorCollectionOptions.GPS_DELAY_MS, 0, locationEndpoint, looper);
+        Criteria c = new Criteria();
+        c.setAccuracy(Criteria.ACCURACY_FINE);
+        c.setBearingAccuracy(Criteria.ACCURACY_HIGH);
+        c.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        c.setSpeedAccuracy(Criteria.ACCURACY_HIGH);
+        c.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+        GlobalContext.getLocationManager().requestSingleUpdate(c, locationEndpoint, looper);
+        GlobalContext.getLocationManager().requestLocationUpdates(SensorCollectionOptions.GPS_DELAY_MS, 0, c, locationEndpoint, looper);
     }
 
     @Override
@@ -54,26 +63,27 @@ public class LocationHolderAndroid extends LocationHolder
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-
+            Log.d(LOG_TAG, "Status changed: " + status);
         }
 
         @Override
         public void onProviderEnabled(String provider) {
+            Log.d(LOG_TAG, "Provider enabled: " + provider);
 
         }
 
         @Override
         public void onProviderDisabled(String provider) {
-
+            Log.d(LOG_TAG, "Provider disabled: " + provider);
         }
     };
 
-    private void checkEnableAndroidGPS(){
-        if(!SensorCollectionOptions.ASK_GPS) return;
+    private void checkEnableAndroidGPS() {
+        if (!SensorCollectionOptions.ASK_GPS) return;
 
         final boolean gps_enabled = GlobalContext.getLocationManager().isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        if(!gps_enabled) {
+        if (!gps_enabled) {
             Toast toast = Toast.makeText(GlobalContext.context, "Please enable location services.", Toast.LENGTH_SHORT);
             toast.show();
 
--- server/UploadServlet/src/main/java/eu/liveandgov/wp1/server/db_helper/inserter/GpsInserter.java
+++ server/UploadServlet/src/main/java/eu/liveandgov/wp1/server/db_helper/inserter/GpsInserter.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
  * Time: 2:45 PM
  * To change this template use File | Settings | File Templates.
  */
-public class GpsInserter extends AbstractInserter<GPS>{
+public class GpsInserter extends AbstractInserter<GPS> {
 
     public GpsInserter(PostgresqlDatabase db) throws SQLException {
         super(db);
@@ -26,7 +26,7 @@ public class GpsInserter extends AbstractInserter<GPS>{
 
     @Override
     protected String getSchema() {
-        return "(trip_id INT, ts BIGINT, lonlat GEOGRAPHY(Point))";
+        return "(trip_id INT, ts BIGINT, lonlat GEOGRAPHY(Point)),altitude FLOAT NULL";
     }
 
     @Override
@@ -39,6 +39,9 @@ public class GpsInserter extends AbstractInserter<GPS>{
         insertStatement.setInt(1, tripId);
         insertStatement.setLong(2, gsv.getTimestamp());
         insertStatement.setString(3, "POINT(" + Double.toString(gsv.lon) + ' ' + Double.toString(gsv.lat) + ")");
+
+        if (gsv.alt != null)
+            insertStatement.setDouble(4, gsv.alt);
     }
 
 }
