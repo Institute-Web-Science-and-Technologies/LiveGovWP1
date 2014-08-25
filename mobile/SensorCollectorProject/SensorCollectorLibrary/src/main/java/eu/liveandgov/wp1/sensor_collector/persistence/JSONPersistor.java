3ec3fd84107723f5cf0cefde8799fa36212afb56
Christoph <cehlen@uni-koblenz.de>
2014-08-26 00:09:53 +0200

    Added support for the new json based streaming format

--- mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/persistence/JSONPersistor.java
+++ mobile/SensorCollectorProject/SensorCollectorLibrary/src/main/java/eu/liveandgov/wp1/sensor_collector/persistence/JSONPersistor.java
@@ -7,6 +7,7 @@ import android.util.JsonWriter;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -127,7 +128,7 @@ public class JSONPersistor implements Persistor {
             }
 
             if (logFile.length() > 1) {
-                raf.writeBytes(",");
+                raf.writeBytes(",\n");
             }
 
             System.out.println(serialize(item));
@@ -186,74 +187,98 @@ public class JSONPersistor implements Persistor {
     }
 
     private String serialize(Item item) throws IllegalArgumentException {
-        JSONArray json = new JSONArray();
-        json.put(item.getType());
-        json.put(item.getTimestamp());
-        json.put(item.getDevice());
-        if (item instanceof Motion) {
-            Motion m = (Motion) item;
-            json.put(m.values);
-        } else if (item instanceof Activity) {
-            Activity a = (Activity) item;
-            JSONArray act = new JSONArray();
-            act.put(a.activity);
-            json.put(act);
-        } else if (item instanceof Bluetooth) {
-            throw new UnsupportedOperationException();
-        } else if (item instanceof GoogleActivity) {
-            GoogleActivity ga = (GoogleActivity) item;
-            JSONArray act = new JSONArray();
-            act.put(ga.activity);
-            act.put(ga.confidence);
-            json.put(act);
-        } else if (item instanceof GPS) {
-            GPS g = (GPS) item;
-            JSONArray gps = new JSONArray();
-            try {
+        try {
+            JSONObject json = new JSONObject();
+
+            json.put("type", item.getType());
+            json.put("ts", item.getTimestamp());
+            json.put("user", item.getDevice());
+            if (item instanceof Motion) {
+                Motion m = (Motion) item;
+                json.put("value", m.values);
+            } else if (item instanceof Activity) {
+                Activity a = (Activity) item;
+                json.put("value", a.activity);
+            } else if (item instanceof Bluetooth) {
+                Bluetooth b = (Bluetooth) item;
+                JSONArray bluetooth = new JSONArray();
+                for (Bluetooth.Item i : b.items) {
+                    JSONObject obj = new JSONObject();
+                    obj.put("address", i.address);
+                    obj.put("bondState", i.bondState);
+                    obj.put("deviceMajorClass", i.deviceMajorClass);
+                    obj.put("deviceMinorClass", i.deviceMinorClass);
+                    obj.put("name", i.name);
+                    obj.put("rssi", i.rssi);
+                    bluetooth.put(obj);
+                }
+                json.put("value", bluetooth);
+            } else if (item instanceof GoogleActivity) {
+                GoogleActivity ga = (GoogleActivity) item;
+                JSONObject act = new JSONObject();
+                act.put("activity", ga.activity);
+                act.put("confidence", ga.confidence);
+                json.put("value", act);
+            } else if (item instanceof GPS) {
+                GPS g = (GPS) item;
+                JSONArray gps = new JSONArray();
+
                 gps.put(g.lat);
                 gps.put(g.lon);
                 gps.put(g.alt);
-            } catch (JSONException e) {
-                // Why is this exception here and nowhere else...
-                e.printStackTrace();
+
+                json.put("value", gps);
+            } else if (item instanceof GSM) {
+                GSM g = (GSM) item;
+                JSONArray gsm = new JSONArray();
+                for (GSM.Item i : g.items) {
+                    JSONObject obj = new JSONObject();
+                    obj.put("rssi", i.rssi);
+                    obj.put("cellIdentity", i.cellIdentity);
+                    obj.put("cellType", i.cellType);
+                    gsm.put(obj);
+                }
+                json.put("value", gsm);
+            } else if (item instanceof WiFi) {
+                WiFi w = (WiFi) item;
+                JSONArray wifi = new JSONArray();
+                for (WiFi.Item i : w.items) {
+                    JSONObject obj = new JSONObject();
+                    obj.put("bssid", i.bssid);
+                    obj.put("frequenct", i.frequency);
+                    obj.put("level", i.level);
+                    obj.put("ssid", i.ssid);
+                    wifi.put(obj);
+                }
+                json.put("value", wifi);
+            } else if (item instanceof Tag) {
+                Tag t = (Tag) item;
+                json.put("value", t.tag);
+            } else if (item instanceof Proximity) {
+                Proximity p = (Proximity) item;
+                JSONObject proximity = new JSONObject();
+                proximity.put("key", p.key);
+                proximity.put("in", p.in);
+                proximity.put("of", p.of);
+                json.put("value", proximity);
+            } else if (item instanceof Waiting) {
+                Waiting w = (Waiting) item;
+                JSONObject waiting = new JSONObject();
+                waiting.put("key", w.key);
+                waiting.put("duration", w.duration);
+                waiting.put("at", w.at);
+                json.put("value", waiting);
+            } else if (item instanceof Velocity) {
+                Velocity v = (Velocity) item;
+                json.put("value", v.velocity);
+            } else {
+                throw new IllegalArgumentException();
             }
-            json.put(gps);
-        } else if (item instanceof GSM) {
-            throw  new UnsupportedOperationException();
-        } else if (item instanceof WiFi) {
-            throw new UnsupportedOperationException();
-        } else if (item instanceof Tag) {
-            Tag t = (Tag) item;
-            JSONArray tag = new JSONArray();
-            tag.put(t.tag);
-            json.put(tag);
-        } else if (item instanceof Proximity) {
-            Proximity p = (Proximity) item;
-            JSONArray proximity = new JSONArray();
-            proximity.put(p.key);
-            proximity.put(p.in);
-            proximity.put(p.of);
-            json.put(proximity);
-        } else if (item instanceof Waiting) {
-            Waiting w = (Waiting) item;
-            JSONArray waiting = new JSONArray();
-            waiting.put(w.key);
-            waiting.put(w.duration);
-            waiting.put(w.at);
-            json.put(waiting);
-        } else if (item instanceof Velocity) {
-            Velocity v = (Velocity) item;
-            JSONArray velocity = new JSONArray();
-            try {
-                velocity.put(v.velocity);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            json.put(velocity);
-        } else {
-            throw new IllegalArgumentException();
+            return json.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
         }
-        return json.toString();
     }
 
     @Override
