1289d9bf799d63a38912e926926d2620bc168ced
Heinrich Hartmann <hartmann@uni-koblenz.de>
2014-09-09 15:10:35 +0200

    Centralized Configuration for UploadServlet.

---
+++ server/UploadServlet/src/main/java/eu/liveandgov/wp1/server/CONFIG.java
@@ -0,0 +1,15 @@
+package eu.liveandgov.wp1.server;
+
+/**
+ * Created by hartmann on 9/9/14.
+ */
+public class CONFIG {
+    static final String OUT_DIR = "/srv/liveandgov/UploadServletRawFiles/";
+    public static final String LOG_FILE = "/var/log/UploadServlet.log";
+
+
+    public static final String DB_NAME = "liveandgov_dev";
+    public static final String DB_USER = "liveandgov";
+    public static final String DB_PASS = "liveandgov";
+
+}
