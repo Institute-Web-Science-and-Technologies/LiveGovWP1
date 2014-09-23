b6c0f95589e2c3d1abd225429954ea1264150cf2
Heinrich Hartmann <hartmann@uni-koblenz.de>
2014-09-23 10:20:05 +0200

    Removed user ID generation from Upload Servlet

--- server/UploadServlet/src/main/java/eu/liveandgov/wp1/server/CONFIG.java
+++ server/UploadServlet/src/main/java/eu/liveandgov/wp1/server/CONFIG.java
@@ -7,7 +7,6 @@ public class CONFIG {
     static final String OUT_DIR = "/srv/liveandgov/UploadServletRawFiles/";
     public static final String LOG_FILE = "/var/log/UploadServlet.log";
 
-
     public static final String DB_NAME = "liveandgov_dev";
     public static final String DB_USER = "liveandgov";
     public static final String DB_PASS = "liveandgov";
